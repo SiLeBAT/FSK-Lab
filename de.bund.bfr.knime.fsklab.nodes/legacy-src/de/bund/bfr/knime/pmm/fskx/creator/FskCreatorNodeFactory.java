e6abf7306a26057d6d28bb2a73158e438dd9ce8f
miguelalba <Miguel.de-Alba-Aparicio@bfr.bund.de>
2016-11-15 16:44:10 +0100

    Simplify legacy factory classes extending the original ones

--- de.bund.bfr.knime.fsklab.nodes/legacy-src/de/bund/bfr/knime/pmm/fskx/creator/FskCreatorNodeFactory.java
+++ de.bund.bfr.knime.fsklab.nodes/legacy-src/de/bund/bfr/knime/pmm/fskx/creator/FskCreatorNodeFactory.java
@@ -1,41 +1,4 @@
 package de.bund.bfr.knime.pmm.fskx.creator;
 
-import org.knime.core.node.NodeDialogPane;
-import org.knime.core.node.NodeFactory;
-import org.knime.core.node.NodeView;
-
-import de.bund.bfr.knime.fsklab.nodes.creator.FskCreatorNodeModel;
-import de.bund.bfr.knime.fsklab.nodes.creator.FskCreatorNodeDialog;
-
-public class FskCreatorNodeFactory extends NodeFactory<FskCreatorNodeModel> {
-  
-  /** {@inheritDoc} */
-  @Override
-  public FskCreatorNodeModel createNodeModel() {
-    return new FskCreatorNodeModel();
-  }
-  
-  /** {@inheritDoc} */
-  @Override
-  public int getNrNodeViews() {
-    return 0;
-  }
-  
-  /** {@inheritDoc} */
-  @Override
-  public NodeView<FskCreatorNodeModel> createNodeView(final int viewIndex, final FskCreatorNodeModel nodeModel) {
-    return null;
-  }
-  
-  /** {@inheritDoc} */
-  @Override
-  public boolean hasDialog() {
-    return true;
-  }
-  
-  /** {@inheritDoc} */
-  @Override
-  public NodeDialogPane createNodeDialogPane() {
-    return new FskCreatorNodeDialog();
-  }
+public class FskCreatorNodeFactory extends de.bund.bfr.knime.fsklab.nodes.creator.FskCreatorNodeFactory {
 }
--- de.bund.bfr.knime.fsklab.nodes/legacy-src/de/bund/bfr/knime/pmm/fskx/runner/FskRunnerNodeFactory.java
+++ de.bund.bfr.knime.fsklab.nodes/legacy-src/de/bund/bfr/knime/pmm/fskx/runner/FskRunnerNodeFactory.java
@@ -1,38 +1,4 @@
 package de.bund.bfr.knime.pmm.fskx.runner;
 
-import org.knime.core.node.NodeDialogPane;
-import org.knime.core.node.NodeFactory;
-import org.knime.core.node.NodeView;
-import org.knime.core.node.defaultnodesettings.DefaultNodeSettingsPane;
-
-import de.bund.bfr.knime.fsklab.nodes.runner.FskRunnerNodeModel;
-import de.bund.bfr.knime.fsklab.nodes.runner.FskRunnerNodeView;
-
-public class FskRunnerNodeFactory extends NodeFactory<FskRunnerNodeModel> {
-
-  @Override
-  public FskRunnerNodeModel createNodeModel() {
-    return new FskRunnerNodeModel();
-  }
-
-  @Override
-  protected int getNrNodeViews() {
-    return 1;
-  }
-
-  @Override
-  public NodeView<FskRunnerNodeModel> createNodeView(int viewIndex,
-      FskRunnerNodeModel nodeModel) {
-    return new FskRunnerNodeView(nodeModel);
-  }
-
-  @Override
-  protected boolean hasDialog() {
-    return true;
-  }
-
-  @Override
-  protected NodeDialogPane createNodeDialogPane() {
-    return new DefaultNodeSettingsPane();
-  }
+public class FskRunnerNodeFactory extends de.bund.bfr.knime.fsklab.nodes.runner.FskRunnerNodeFactory {
 }
