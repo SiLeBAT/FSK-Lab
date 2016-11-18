1264591bdaac11b0b3d06a71456794efb0a9ab64
miguelalba <Miguel.de-Alba-Aparicio@bfr.bund.de>
2016-11-18 14:59:40 +0100

    Fix URISTest

--- de.bund.bfr.knime.fsklab.nodes/test/de/bund/bfr/knime/nodes/URISTest.java
+++ de.bund.bfr.knime.fsklab.nodes/test/de/bund/bfr/knime/nodes/URISTest.java
@@ -2,6 +2,9 @@ package de.bund.bfr.knime.nodes;
 
 import static org.junit.Assert.*;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import org.junit.Test;
 
 import de.bund.bfr.knime.fsklab.nodes.URIS;
@@ -9,12 +12,12 @@ import de.bund.bfr.knime.fsklab.nodes.URIS;
 public class URISTest {
 
 	@Test
-	public void test() {
-		assertEquals("http://purl.org/NET/mediatypes/application/zip", URIS.zip);
-		assertEquals("http://purl.org/NET/mediatypes/application/r", URIS.r);
-		assertEquals("http://purl.org/NET/mediatypes/application/x-pmf", URIS.pmf);
-		assertEquals("http://purl.org/NET/mediatypes/application/sbml+xml", URIS.sbml);
-		assertEquals("http://purl.org/NET/mediatypes/text/x-matlab", URIS.matlab);
-		assertEquals("http://purl.org/NET/mediatypes/text/x-php", URIS.php);
+	public void test() throws URISyntaxException {
+		assertEquals(new URI("http://purl.org/NET/mediatypes/application/zip"), URIS.zip);
+		assertEquals(new URI("http://purl.org/NET/mediatypes/application/r"), URIS.r);
+		assertEquals(new URI("http://purl.org/NET/mediatypes/application/x-pmf"), URIS.pmf);
+		assertEquals(new URI("http://purl.org/NET/mediatypes/application/sbml+xml"), URIS.sbml);
+		assertEquals(new URI("http://purl.org/NET/mediatypes/text/x-matlab"), URIS.matlab);
+		assertEquals(new URI("http://purl.org/NET/mediatypes/text/x-php"), URIS.php);
 	}
 }
