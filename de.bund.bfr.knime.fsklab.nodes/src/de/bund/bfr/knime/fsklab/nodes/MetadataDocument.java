8bfe680dfa3e873367ffb3facac60f477e6468fd
miguelalba <Miguel.de-Alba-Aparicio@bfr.bund.de>
2016-11-25 12:51:15 +0100

    Integrated MetadataAnnotation into MetadataDocument

--- de.bund.bfr.knime.fsklab.nodes/src/de/bund/bfr/knime/fsklab/nodes/MetadataDocument.java
+++ de.bund.bfr.knime.fsklab.nodes/src/de/bund/bfr/knime/fsklab/nodes/MetadataDocument.java
@@ -2,6 +2,7 @@ package de.bund.bfr.knime.fsklab.nodes;
 
 import java.text.ParseException;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
@@ -26,8 +27,11 @@ import org.sbml.jsbml.util.filters.Filter;
 import org.sbml.jsbml.xml.XMLNode;
 import org.sbml.jsbml.xml.XMLTriple;
 
+import com.google.common.base.Strings;
+
 import de.bund.bfr.knime.fsklab.nodes.FskMetaData.DataType;
 import de.bund.bfr.pmfml.ModelClass;
+import de.bund.bfr.pmfml.ModelType;
 import de.bund.bfr.pmfml.PMFUtil;
 import de.bund.bfr.pmfml.sbml.Limits;
 import de.bund.bfr.pmfml.sbml.LimitsConstraint;
@@ -38,7 +42,7 @@ import de.bund.bfr.pmfml.sbml.SBMLFactory;
 public class MetadataDocument {
 
 	public SBMLDocument doc;
-	
+
 	public MetadataDocument(FskMetaData template) {
 		// Creates SBMLDocument for the primary model
 		doc = new SBMLDocument(3, 1);
@@ -46,8 +50,7 @@ public class MetadataDocument {
 		// Adds namespaces to the sbmlDocument
 		doc.addDeclaredNamespace("xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance");
 		doc.addDeclaredNamespace("xmlns:pmml", "http://www.dmg.org/PMML-4_2");
-		doc.addDeclaredNamespace("xmlns:pmf",
-				"http://sourceforge.net/projects/microbialmodelingexchange/files/PMF-ML");
+		doc.addDeclaredNamespace("xmlns:pmf", "http://sourceforge.net/projects/microbialmodelingexchange/files/PMF-ML");
 		doc.addDeclaredNamespace("xmlns:dc", "http://purl.org/dc/elements/1.1");
 		doc.addDeclaredNamespace("xmlns:dcterms", "http://purl.org/dc/terms/");
 		doc.addDeclaredNamespace("xmlns:pmmlab",
@@ -131,11 +134,11 @@ public class MetadataDocument {
 				// TODO: Add array
 				try {
 					param.setValue(0);
-					addArrayToParameter(param, v.value, v.name);	
+					addArrayToParameter(param, v.value, v.name);
 				} catch (ParseException e) {
 					e.printStackTrace();
 				}
-				
+
 				break;
 			case character:
 				// TODO: Add character
@@ -167,48 +170,7 @@ public class MetadataDocument {
 		rule.setAnnotation(new RuleAnnotation(modelClass).annotation);
 		model.addRule(rule);
 	}
-	
-	
-	/** Reduced version of PMF-ML ModelRuleAnnotation with only the model class. */
-	public class RuleAnnotation {
-		
-		public static final String SUBJECT_NAME = "subject";
-		public static final String SUBJECT_URI = "pmmlab";
-		
-		public ModelClass modelClass;
-		public Annotation annotation;
-		
-		public RuleAnnotation(final ModelClass modelClass) {
-			// Builds metadata node
-			XMLTriple pmfTriple = new XMLTriple("metadata", null, "pmf");
-			XMLNode pmfNode = new XMLNode(pmfTriple);
-			
-			// Builds model class node
-			XMLTriple modelClassTriple = new XMLTriple(SUBJECT_NAME, null, SUBJECT_URI);
-			XMLNode modelClassNode = new XMLNode(modelClassTriple);
-			modelClassNode.addChild(new XMLNode(modelClass.fullName()));
-			pmfNode.addChild(modelClassNode);
-			
-			// Create annotation
-			annotation = new Annotation();
-			annotation.setNonRDFAnnotation(pmfNode);
-		}
-		
-		public RuleAnnotation(final Annotation annotation) {
-			
-			XMLNode pmfNode = annotation.getNonRDFannotation().getChildElement("metadata", "");
-			
-			// Reads model class node
-			XMLNode modelClassNode = pmfNode.getChildElement(SUBJECT_NAME, "");
-			if (modelClassNode != null) {
-				modelClass = ModelClass.fromName(modelClassNode.getChild(0).getCharacters());
-			}
-			
-			// Copies annotation
-			this.annotation = annotation;
-		}
-	}
-	
+
 	private static void addArrayToParameter(Parameter parameter, String value, String var) throws ParseException {
 		String cleanArray = value.substring(2, value.length() - 1);
 		String[] tokens = cleanArray.split(",");
@@ -224,26 +186,26 @@ public class MetadataDocument {
 		// Create initial assignment
 		InitialAssignment ia = parameter.getModel().createInitialAssignment();
 		ia.setVariable(var);
-		
+
 		// Create math of initial assignment with a selector function
 		ia.setMath(new SelectorNode(array, ia).node);
 
 		ArraysSBasePlugin iaPlugin = (ArraysSBasePlugin) ia.getPlugin(ArraysConstants.shortLabel);
-		
+
 		// Add dimension to initial assignment
 		iaPlugin.addDimension(dim.clone());
-		
+
 		// Add index to initial assignment
 		Index index = iaPlugin.createIndex();
 		index.setReferencedAttribute("symbol");
 		index.setArrayDimension(0);
 		index.setMath(new ASTNode("d0"));
 	}
-	
+
 	public MetadataDocument(final SBMLDocument doc) {
 		this.doc = doc;
 	}
-	
+
 	public FskMetaData getMetaData() {
 		FskMetaData template = new FskMetaData();
 
@@ -382,7 +344,223 @@ public class MetadataDocument {
 		}
 
 		template.hasData = false;
-		
+
 		return template;
+	}
+
+	public class MetadataAnnotation {
+
+		private static final String METADATA_TAG = "metadata"; // Metadata tag
+		private static final String METADATA_NS = "pmf"; // Metadata namespace
+
+		// Namespace and tag for the creator: <dc:creator>
+		private final static String CREATOR_NS = "dc";
+		private final static String CREATOR_TAG = "creator";
+
+		// Namespace and tag for the created date: <dcterms:created>
+		private final static String CREATED_NS = "dcterms";
+		private final static String CREATED_TAG = "created";
+
+		// Namespace and tag for the modified date: <dcterms:modified>
+		private final static String MODIFIED_NS = "dcterms";
+		private final static String MODIFIED_TAG = "modified";
+
+		// Namespace and tag for the type: <dc:type>
+		private final static String TYPE_NS = "dc";
+		private final static String TYPE_TAG = "type";
+
+		// Namespace and tag for the rights: <dc:rights>
+		private final static String RIGHTS_NS = "dc";
+		private final static String RIGHTS_TAG = "rights";
+
+		// Namespace and tag for the reference description: <dc:description>
+		private final static String REFDESC_NS = "dc";
+		private final static String REFDESC_TAG = "description";
+
+		// Namespace and tag for the reference description link: <dc:source>
+		private final static String REFDESCLINK_NS = "dc";
+		private final static String REFDESCLINK_TAG = "source";
+
+		public Annotation annotation;
+
+		// Fields with metadata
+		public String givenName;
+		public String familyName;
+		public String contact;
+		public Date createdDate;
+		public Date modifiedDate;
+		public ModelType type;
+		public String rights;
+		public String referenceDescription;
+		public String referenceDescriptionLink;
+
+		public MetadataAnnotation(final FskMetaData metadata) {
+
+			XMLTriple pmfTriple = new XMLTriple(METADATA_TAG, "", METADATA_NS);
+			XMLNode pmfNode = new XMLNode(pmfTriple);
+
+			// Builds creator node
+			if (!Strings.isNullOrEmpty(metadata.creator) || !Strings.isNullOrEmpty(metadata.familyName)
+					|| !Strings.isNullOrEmpty(metadata.contact)) {
+				givenName = Strings.nullToEmpty(metadata.creator);
+				familyName = Strings.nullToEmpty(metadata.familyName);
+				contact = Strings.nullToEmpty(metadata.contact);
+
+				String creator = givenName + "." + familyName + "." + contact;
+				XMLNode creatorNode = new XMLNode(new XMLTriple(CREATOR_TAG, null, CREATOR_NS));
+				creatorNode.addChild(new XMLNode(creator));
+				pmfNode.addChild(creatorNode);
+			}
+
+			// Builds created date node
+			if (metadata.createdDate != null) {
+				XMLNode createdNode = new XMLNode(new XMLTriple(CREATED_TAG, "", CREATED_NS));
+				createdNode.addChild(new XMLNode(FskMetaData.dateFormat.format(metadata.createdDate)));
+				pmfNode.addChild(createdNode);
+			}
+
+			// Builds modified date node
+			if (metadata.modifiedDate != null) {
+				XMLNode modifiedNode = new XMLNode(new XMLTriple(MODIFIED_TAG, "", MODIFIED_NS));
+				modifiedNode.addChild(new XMLNode(FskMetaData.dateFormat.format(metadata.modifiedDate)));
+				pmfNode.addChild(modifiedNode);
+			}
+
+			// Builds type node
+			if (metadata.type != null) {
+				XMLNode typeNode = new XMLNode(new XMLTriple(TYPE_TAG, "", TYPE_NS));
+				typeNode.addChild(new XMLNode(metadata.type.name()));
+				pmfNode.addChild(typeNode);
+			}
+
+			// Builds rights node
+			if (!Strings.isNullOrEmpty(metadata.rights)) {
+				XMLNode rightsNode = new XMLNode(new XMLTriple(RIGHTS_TAG, "", RIGHTS_NS));
+				rightsNode.addChild(new XMLNode(metadata.rights));
+				pmfNode.addChild(rightsNode);
+			}
+
+			// Builds reference description node
+			if (!Strings.isNullOrEmpty(metadata.referenceDescription)) {
+				XMLNode refdescNode = new XMLNode(new XMLTriple(REFDESC_TAG, "", REFDESC_NS));
+				refdescNode.addChild(new XMLNode(metadata.referenceDescription));
+				pmfNode.addChild(refdescNode);
+			}
+
+			// Builds reference description link node
+			if (!Strings.isNullOrEmpty(metadata.referenceDescriptionLink)) {
+				XMLNode refdescLinkNode = new XMLNode(new XMLTriple(REFDESCLINK_TAG, "", REFDESCLINK_NS));
+				refdescLinkNode.addChild(new XMLNode(metadata.referenceDescriptionLink));
+				pmfNode.addChild(refdescLinkNode);
+			}
+
+			// Create annotation
+			annotation = new Annotation();
+			annotation.setNonRDFAnnotation(pmfNode);
+		}
+
+		public MetadataAnnotation(final Annotation annotation) {
+			XMLNode pmfNode = annotation.getNonRDFannotation().getChildElement(METADATA_TAG, "");
+
+			// Reads creatorNode
+			XMLNode creatorNode = pmfNode.getChildElement(CREATOR_TAG, "");
+			if (creatorNode != null) {
+				String[] tempStrings = creatorNode.getChild(0).getCharacters().split("\\.", 3);
+				givenName = Strings.nullToEmpty(tempStrings[0]);
+				familyName = Strings.nullToEmpty(tempStrings[1]);
+				contact = Strings.nullToEmpty(tempStrings[2]);
+			}
+
+			// Reads created date
+			XMLNode createdNode = pmfNode.getChildElement(CREATED_TAG, "");
+			if (createdNode != null) {
+				try {
+					createdDate = FskMetaData.dateFormat.parse(createdNode.getChild(0).getCharacters());
+				} catch (ParseException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+
+			// Reads modified date
+			XMLNode modifiedNode = pmfNode.getChildElement(MODIFIED_TAG, "");
+			if (modifiedNode != null) {
+				try {
+					modifiedDate = FskMetaData.dateFormat.parse(modifiedNode.getChild(0).getCharacters());
+				} catch (ParseException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+
+			// Reads model type
+			XMLNode typeNode = pmfNode.getChildElement(TYPE_TAG, "");
+			if (typeNode != null) {
+				type = ModelType.valueOf(typeNode.getChild(0).getCharacters());
+			}
+
+			// Reads rights
+			XMLNode rightsNode = pmfNode.getChildElement(RIGHTS_TAG, "");
+			if (rightsNode != null) {
+				rights = rightsNode.getChild(0).getCharacters();
+			}
+
+			// Reads reference description
+			XMLNode refdescNode = pmfNode.getChildElement(REFDESC_TAG, "");
+			if (refdescNode != null) {
+				referenceDescription = refdescNode.getChild(0).getCharacters();
+			}
+
+			// Reads reference description link
+			XMLNode refdescLinkNode = pmfNode.getChildElement(REFDESCLINK_TAG, "");
+			if (refdescLinkNode != null) {
+				referenceDescriptionLink = refdescLinkNode.getChild(0).getCharacters();
+			}
+
+			// Copies annotation
+			this.annotation = annotation;
+		}
+	}
+
+	/**
+	 * Reduced version of PMF-ML ModelRuleAnnotation with only the model class.
+	 */
+	public class RuleAnnotation {
+
+		public static final String SUBJECT_NAME = "subject";
+		public static final String SUBJECT_URI = "pmmlab";
+
+		public ModelClass modelClass;
+		public Annotation annotation;
+
+		public RuleAnnotation(final ModelClass modelClass) {
+			// Builds metadata node
+			XMLTriple pmfTriple = new XMLTriple("metadata", null, "pmf");
+			XMLNode pmfNode = new XMLNode(pmfTriple);
+
+			// Builds model class node
+			XMLTriple modelClassTriple = new XMLTriple(SUBJECT_NAME, null, SUBJECT_URI);
+			XMLNode modelClassNode = new XMLNode(modelClassTriple);
+			modelClassNode.addChild(new XMLNode(modelClass.fullName()));
+			pmfNode.addChild(modelClassNode);
+
+			// Create annotation
+			annotation = new Annotation();
+			annotation.setNonRDFAnnotation(pmfNode);
+		}
+
+		public RuleAnnotation(final Annotation annotation) {
+
+			XMLNode pmfNode = annotation.getNonRDFannotation().getChildElement("metadata", "");
+
+			// Reads model class node
+			XMLNode modelClassNode = pmfNode.getChildElement(SUBJECT_NAME, "");
+			if (modelClassNode != null) {
+				modelClass = ModelClass.fromName(modelClassNode.getChild(0).getCharacters());
+			}
+
+			// Copies annotation
+			this.annotation = annotation;
+		}
 	}
 }
