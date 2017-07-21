8dfee90742ed28f77d2a97348a14d7314b03be51
miguelalba <miguel.de-alba-aparicio@bfr.bund.de>
2017-07-21 10:50:22 +0200

    Remove print and redundant public modifiers in FskPlugin

--- de.bund.bfr.knime.fsklab.nodes.kotlin/src/de/bund/bfr/knime/fsklab/FskPlugin.kt
+++ de.bund.bfr.knime.fsklab.nodes.kotlin/src/de/bund/bfr/knime/fsklab/FskPlugin.kt
@@ -4,20 +4,17 @@ import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 import javax.swing.JOptionPane
 
-public class FskPlugin : AbstractUIPlugin() {
+class FskPlugin : AbstractUIPlugin() {
 	
-	init {
-		print("jfakjfakljfakjfaklfjakjfl")
-	}
 	/**
 	 * This method is called upon plug-in activation.
      * @param context The OSGI bundle context
 	 */
-	public override fun start(context: BundleContext) = super.start(context)
+	override fun start(context: BundleContext) = super.start(context)
 	
 	/**
 	 * This method is called when the plug-in is stopped.
 	 * @param context The OSGI bundle context
 	 */
-	public override fun stop(context: BundleContext) = super.stop(context)
+	override fun stop(context: BundleContext) = super.stop(context)
 }
