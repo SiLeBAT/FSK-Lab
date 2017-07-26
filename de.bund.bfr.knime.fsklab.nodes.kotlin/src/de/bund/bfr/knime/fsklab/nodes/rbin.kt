0b2bd9b9b55aa90a7e8aee7728dc12b5d72865ae
miguelalba <Miguel.de-Alba-Aparicio@bfr.bund.de>
2017-07-26 13:20:29 +0200

    RBin preference (Kotlin)

--- de.bund.bfr.knime.fsklab.nodes.kotlin/src/de/bund/bfr/knime/fsklab/nodes/rbin.kt
+++ de.bund.bfr.knime.fsklab.nodes.kotlin/src/de/bund/bfr/knime/fsklab/nodes/rbin.kt
@@ -3,6 +3,13 @@ package de.bund.bfr.knime.fsklab.nodes
 import com.sun.jna.Platform
 import de.bund.bfr.knime.fsklab.nodes.rbin.preferences.RPreferenceInitializer
 import de.bund.bfr.knime.fsklab.nodes.rbin.preferences.RPreferenceProvider
+import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer
+import org.eclipse.jface.preference.DirectoryFieldEditor
+import org.eclipse.jface.preference.FieldEditorPreferencePage
+import org.eclipse.jface.resource.JFaceResources
+import org.eclipse.swt.widgets.Composite
+import org.eclipse.ui.IWorkbench
+import org.eclipse.ui.IWorkbenchPreferencePage
 import org.knime.core.node.KNIMEConstants
 import org.knime.core.node.NodeLogger
 import org.knime.core.util.FileUtil
@@ -12,6 +19,8 @@ import java.io.FileInputStream
 import java.io.FileWriter
 import java.io.IOException
 import java.io.InputStreamReader
+import java.nio.file.Files
+import java.nio.file.Paths
 import java.util.Properties
 import kotlin.concurrent.thread
 
@@ -195,4 +204,173 @@ fun checkRHome(rHomePath: String, fromPreferences: Boolean) {
 	}
 }
 
+/**
+ * Class that provides a path to an R home directory
+ * @author Heiko Hofer
+ * @author Jonathan Hale
+ * @author Miguel de Alba
+ */
+interface RPreferenceProvider {
+
+	/** Path to RHome. */
+	fun getRHome(): String
+
+	/**
+	 * @param command R command ("R" or "Rscript" for example)
+	 * @return path to the command executable
+	 */
+	fun getRBinPath(command: String): String
+
+	/** @return path to an Rserve executable */
+	fun getRServeBinPath(): String
+}
+
+/**
+ * Default provider for R preferences. It determines the R binary path based on
+ * the R home given in the constructor.
+ * @property rHome R's home directory
+ *
+ * @author Thorsten Meinl, KNIME.com, Zurich, Switzerland
+ * @author Jonathan Hale
+ * @author Miguel de Alba
+ */
+class DefaultRPreferenceProvider(home: String) : RPreferenceProvider {
+
+	private val rHome = home
+
+	/** Properties for this provider. */
+	val properties by lazy { retrieveRProperties(this) }
+
+	override fun getRHome() = rHome
+
+	override fun getRBinPath(command: String): String {
+
+		val binPath = Paths.get(getRHome(), "bin")
+
+		return if (Platform.isWindows()) {
+			val arch = if (Platform.is64Bit()) "x64" else "i386"
+			"${binPath.resolve(arch)}${File.separator}$command.exe"
+		} else "$binPath{File.separator}$command"
+	}
+
+	override fun getRServeBinPath(): String {
+
+		val rservePath = Paths.get(properties.getProperty("Rserve.path"))
+		val rserveLibs = rservePath.resolve("libs")
+
+		return if (Platform.isWindows()) {
+			val arch = if (Platform.is64Bit()) "x64" else "i386"
+			rserveLibs.resolve("$arch/RServe.exe").toString()
+		} else rserveLibs.resolve("Rserve.dbg").toString()
+	}
+}
+
+val R3_PATH = "r3.path"
+private var cachedR3PreferenceProvider: RPreferenceProvider? = null
+
+/* @return provider to the path to the R3 executable. */
+fun getR3Provider(): RPreferenceProvider {
+
+	val r3Home = FskPlugin.getDefault().preferenceStore.getString(R3_PATH)
+
+	val localProvider = cachedR3PreferenceProvider
+	if (localProvider == null || localProvider.rHome != r3Home) {
+		cachedR3PreferenceProvider = DefaultRPreferenceProvider(r3Home)
+	}
+
+	return cachedR3PreferenceProvider!!
+}
+
+/**
+ * Invalidate the cached R3 preference provider to refetch R properties
+ * (which launches an external R command).
+ */
+fun invalidateR3PreferenceProviderCache() {
+	cachedR3PreferenceProvider = null
+}
+
+/**
+ * Initializes preference page with defualt paths to R2 and R3 environments.
+ * @author Miguel de Alba
+ */
+class RPreferenceInitializer : AbstractPreferenceInitializer() {
+
+	override fun initializeDefaultPreferences(): Unit {
+		FskPlugin.getDefault().preferenceStore.setDefault("R3_PATH", "")
+	}
+}
+
+/**
+ * Preferences page.
+ * @author Miguel de Alba
+ */
+class RPreferencePage : FieldEditorPreferencePage(GRID), IWorkbenchPreferencePage {
+
+	init {
+		preferenceStore = FskPlugin.getDefault().preferenceStore
+		description = "BfR R nodes preferences"
+	}
+
+	override fun createFieldEditors() {
+		addField(RHomeDirectoryFieldEditor(name = R3_PATH, labelText = "R v3 environment location",
+				parent = fieldEditorParent))
+	}
+
+	override fun init(workbench: IWorkbench) = Unit
+
+	private inner class RHomeDirectoryFieldEditor(name: String, labelText: String, parent: Composite) :
+			DirectoryFieldEditor() {
+
+		init {
+			init(name, labelText)
+			setChangeButtonText(JFaceResources.getString("openBrowse"))
+			setValidateStrategy(VALIDATE_ON_KEY_STROKE)
+			createControl(parent)
+		}
+		
+		override fun doCheckState(): Boolean {
+			val rHome = stringValue
+			
+			val rHomePath = Paths.get(rHome)
+			if (!Files.isDirectory(rHomePath)) {
+				setMessage("The selected path is not a directory.", ERROR)
+				return false
+			}
+			
+			try {
+				checkRHome(rHomePath = rHome, fromPreferences = true)
+				
+				val props = DefaultRPreferenceProvider(rHome).properties
+				// the version numbers may contain spaces
+				val version = (props.getProperty("major") + "." + props.getProperty("minor")).replace(" ", "")
+				
+				if ("3.1.0" == version) {
+					setMessage("You have selected an R 3.1.0 installation. " +
+							"Please see http://tech.knime.org/faq#q26 for details.", WARNING)
+					return true
+				}
+				
+				if (props.getProperty("Rserve.path").isNullOrEmpty()) {
+					setMessage("The package 'Rserve' needs to be installed in your R installation. " +
+							"Please install it in R using \"install.packages('Rserve')\".", WARNING)
+					return true
+				}
+				
+				if (Platform.isMac() && props.getProperty("Cairo.path").isNullOrEmpty()) {
+					// Under Mac we need Cairo package to use png()/bmp() devices
+					setMessage("The package 'Cairo' needs to be installed in your R installation for " +
+							"bitmap graphics devices to work properly. Please install it in R using " +
+							"\"install.packages('Cairo')\".", WARNING)
+					return true
+				}
+				
+				setMessage(null, NONE)
+				return true
+			} catch (e: InvalidRHomeException) {
+				setMessage(e.message, ERROR)
+				return false
+			}
+		}
+	}
+}
 
