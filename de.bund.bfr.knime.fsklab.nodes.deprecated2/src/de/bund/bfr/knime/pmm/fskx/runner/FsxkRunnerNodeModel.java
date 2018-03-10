400c873b63c4f57c02f053bac36f55810e79a03b
miguelalba <miguel.de-alba-aparicio@bfr.bund.de>
2018-03-10 11:43:21 +0100

    Fix legacy runner on Mac - Cairo issue
    The Cairo library which is needed on Mac to generate plots was
    not loaded in the legacy runner.

--- de.bund.bfr.knime.fsklab.nodes.deprecated2/src/de/bund/bfr/knime/pmm/fskx/runner/FsxkRunnerNodeModel.java
+++ de.bund.bfr.knime.fsklab.nodes.deprecated2/src/de/bund/bfr/knime/pmm/fskx/runner/FsxkRunnerNodeModel.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.stream.Collectors;
+
 import org.knime.core.data.DataRow;
 import org.knime.core.data.container.CloseableRowIterator;
 import org.knime.core.data.def.StringCell;
@@ -27,6 +28,9 @@ import org.knime.core.util.FileUtil;
 import org.knime.ext.r.node.local.port.RPortObject;
 import org.knime.ext.r.node.local.port.RPortObjectSpec;
 import org.rosuda.REngine.REXPMismatchException;
+
+import com.sun.jna.Platform;
+
 import de.bund.bfr.knime.fsklab.nodes.RunnerNodeInternalSettings;
 import de.bund.bfr.knime.fsklab.nodes.controller.IRController.RException;
 import de.bund.bfr.knime.fsklab.nodes.controller.LibRegistry;
@@ -37,161 +41,168 @@ import de.bund.bfr.knime.pmm.fskx.FskMetaDataTuple;
 
 public class FsxkRunnerNodeModel extends NodeModel {
 
-  private static final NodeLogger LOGGER = NodeLogger.getLogger("Fskx Runner Node Model");
+	private static final NodeLogger LOGGER = NodeLogger.getLogger("Fskx Runner Node Model");
 
-  /** Output spec for an FSK object. */
-  private static final FskPortObjectSpec FSK_SPEC = FskPortObjectSpec.INSTANCE;
+	/** Output spec for an FSK object. */
+	private static final FskPortObjectSpec FSK_SPEC = FskPortObjectSpec.INSTANCE;
 
-  /** Output spec for an R object. */
-  private static final RPortObjectSpec R_SPEC = RPortObjectSpec.INSTANCE;
+	/** Output spec for an R object. */
+	private static final RPortObjectSpec R_SPEC = RPortObjectSpec.INSTANCE;
 
-  /** Output spec for a PNG image. */
-  private static final ImagePortObjectSpec PNG_SPEC = new ImagePortObjectSpec(PNGImageContent.TYPE);
+	/** Output spec for a PNG image. */
+	private static final ImagePortObjectSpec PNG_SPEC = new ImagePortObjectSpec(PNGImageContent.TYPE);
 
-  private static final PortType[] inPortTypes =
-      new PortType[] {FskPortObject.TYPE, BufferedDataTable.TYPE_OPTIONAL};
-  private static final PortType[] outPortTypes =
-      new PortType[] {FskPortObject.TYPE, RPortObject.TYPE, ImagePortObject.TYPE_OPTIONAL};
+	private static final PortType[] inPortTypes = new PortType[] { FskPortObject.TYPE,
+			BufferedDataTable.TYPE_OPTIONAL };
+	private static final PortType[] outPortTypes = new PortType[] { FskPortObject.TYPE, RPortObject.TYPE,
+			ImagePortObject.TYPE_OPTIONAL };
 
-  private final RunnerNodeInternalSettings internalSettings = new RunnerNodeInternalSettings();
+	private final RunnerNodeInternalSettings internalSettings = new RunnerNodeInternalSettings();
 
-  public FsxkRunnerNodeModel() {
-    super(inPortTypes, outPortTypes);
-  }
+	public FsxkRunnerNodeModel() {
+		super(inPortTypes, outPortTypes);
+	}
 
-  // --- internal settings methods ---
+	// --- internal settings methods ---
 
-  @Override
-  protected void loadInternals(File nodeInternDir, ExecutionMonitor exec)
-      throws IOException, CanceledExecutionException {
-    internalSettings.loadInternals(nodeInternDir);
-  }
+	@Override
+	protected void loadInternals(File nodeInternDir, ExecutionMonitor exec)
+			throws IOException, CanceledExecutionException {
+		internalSettings.loadInternals(nodeInternDir);
+	}
 
-  @Override
-  protected void saveInternals(File nodeInternDir, ExecutionMonitor exec)
-      throws IOException, CanceledExecutionException {
-    internalSettings.saveInternals(nodeInternDir);
-  }
+	@Override
+	protected void saveInternals(File nodeInternDir, ExecutionMonitor exec)
+			throws IOException, CanceledExecutionException {
+		internalSettings.saveInternals(nodeInternDir);
+	}
 
-  @Override
-  protected void reset() {
-    internalSettings.reset();
-  }
+	@Override
+	protected void reset() {
+		internalSettings.reset();
+	}
 
-  // --- node settings methods ---
+	// --- node settings methods ---
 
-  @Override
-  protected void saveSettingsTo(NodeSettingsWO settings) {
-    // no settings
-  }
+	@Override
+	protected void saveSettingsTo(NodeSettingsWO settings) {
+		// no settings
+	}
 
-  @Override
-  protected void validateSettings(NodeSettingsRO settings) throws InvalidSettingsException {
-    // no settings
-  }
+	@Override
+	protected void validateSettings(NodeSettingsRO settings) throws InvalidSettingsException {
+		// no settings
+	}
 
-  @Override
-  protected void loadValidatedSettingsFrom(NodeSettingsRO settings)
-      throws InvalidSettingsException {
-    // no settings
-  }
+	@Override
+	protected void loadValidatedSettingsFrom(NodeSettingsRO settings) throws InvalidSettingsException {
+		// no settings
+	}
 
-  @Override
-  protected PortObjectSpec[] configure(PortObjectSpec[] inSpecs) throws InvalidSettingsException {
-    return new PortObjectSpec[] {FSK_SPEC, R_SPEC, PNG_SPEC};
-  }
+	@Override
+	protected PortObjectSpec[] configure(PortObjectSpec[] inSpecs) throws InvalidSettingsException {
+		return new PortObjectSpec[] { FSK_SPEC, R_SPEC, PNG_SPEC };
+	}
 
-  @Override
-  protected PortObject[] execute(PortObject[] inObjects, ExecutionContext exec) throws Exception {
-    exec.checkCanceled();
-    FskPortObject fskObj = (FskPortObject) inObjects[0];
+	@Override
+	protected PortObject[] execute(PortObject[] inObjects, ExecutionContext exec) throws Exception {
+		exec.checkCanceled();
+		FskPortObject fskObj = (FskPortObject) inObjects[0];
 
-    if (!fskObj.template.independentVariables.isEmpty()) {
-      fskObj.param = fskObj.template.independentVariables.stream()
-          .map(v -> v.name + " <- " + v.value).collect(Collectors.joining("\n"));
-    }
+		if (!fskObj.template.independentVariables.isEmpty()) {
+			fskObj.param = fskObj.template.independentVariables.stream().map(v -> v.name + " <- " + v.value)
+					.collect(Collectors.joining("\n"));
+		}
 
-    // If a metadata table is connected then update the model metadata
-    else if (inObjects.length == 2 && inObjects[1] != null) {
-      BufferedDataTable metadataTable = (BufferedDataTable) inObjects[1];
-      if (metadataTable.size() == 1) {
-        try (CloseableRowIterator iterator = metadataTable.iterator()) {
-          DataRow dataRow = iterator.next();
-          iterator.close();
+		// If a metadata table is connected then update the model metadata
+		else if (inObjects.length == 2 && inObjects[1] != null) {
+			BufferedDataTable metadataTable = (BufferedDataTable) inObjects[1];
+			if (metadataTable.size() == 1) {
+				try (CloseableRowIterator iterator = metadataTable.iterator()) {
+					DataRow dataRow = iterator.next();
+					iterator.close();
 
-          // Gets independent variables and their values
-          StringCell varCell =
-              (StringCell) dataRow.getCell(FskMetaDataTuple.Key.indepvars.ordinal());
-          String[] vars = varCell.getStringValue().split("\\|\\|");
+					// Gets independent variables and their values
+					StringCell varCell = (StringCell) dataRow.getCell(FskMetaDataTuple.Key.indepvars.ordinal());
+					String[] vars = varCell.getStringValue().split("\\|\\|");
 
-          StringCell valuesCell =
-              (StringCell) dataRow.getCell(FskMetaDataTuple.Key.indepvars_values.ordinal());
-          String[] values = valuesCell.getStringValue().split("\\|\\|");
+					StringCell valuesCell = (StringCell) dataRow
+							.getCell(FskMetaDataTuple.Key.indepvars_values.ordinal());
+					String[] values = valuesCell.getStringValue().split("\\|\\|");
 
-          if (vars != null && values != null && vars.length == values.length) {
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < vars.length; i++) {
-              sb.append(vars[i] + " <- " + values[i] + "\n");
-            }
-            fskObj.param = sb.toString();
-          }
-        }
-      }
-    }
+					if (vars != null && values != null && vars.length == values.length) {
+						StringBuilder sb = new StringBuilder();
+						for (int i = 0; i < vars.length; i++) {
+							sb.append(vars[i] + " <- " + values[i] + "\n");
+						}
+						fskObj.param = sb.toString();
+					}
+				}
+			}
+		}
 
-    try (RController controller = new RController()) {
-      fskObj = runSnippet(controller, (FskPortObject) inObjects[0]);
-    }
-    RPortObject rObj = new RPortObject(fskObj.workspace);
+		try (RController controller = new RController()) {
+			fskObj = runSnippet(controller, (FskPortObject) inObjects[0]);
+		}
+		RPortObject rObj = new RPortObject(fskObj.workspace);
 
-    try (FileInputStream fis = new FileInputStream(internalSettings.imageFile)) {
-      final PNGImageContent content = new PNGImageContent(fis);
-      internalSettings.plot = content.getImage();
-      ImagePortObject imgObj = new ImagePortObject(content, PNG_SPEC);
-      return new PortObject[] {fskObj, rObj, imgObj};
-    } catch (IOException e) {
-      LOGGER.warn("There is no image created");
-      return new PortObject[] {fskObj, rObj};
-    }
-  }
+		try (FileInputStream fis = new FileInputStream(internalSettings.imageFile)) {
+			final PNGImageContent content = new PNGImageContent(fis);
+			internalSettings.plot = content.getImage();
+			ImagePortObject imgObj = new ImagePortObject(content, PNG_SPEC);
+			return new PortObject[] { fskObj, rObj, imgObj };
+		} catch (IOException e) {
+			LOGGER.warn("There is no image created");
+			return new PortObject[] { fskObj, rObj };
+		}
+	}
 
-  private FskPortObject runSnippet(final RController controller, final FskPortObject fskObj)
-      throws IOException, RException, REXPMismatchException {
+	private FskPortObject runSnippet(final RController controller, final FskPortObject fskObj)
+			throws IOException, RException, REXPMismatchException {
 
-    // Add path
-    LibRegistry libRegistry = LibRegistry.instance();
-    String cmd = ".libPaths(c(\"" + libRegistry.getInstallationPath().toString().replace("\\", "/")
-        + "\", .libPaths()))";
-    String[] newPaths = controller.eval(cmd, true).asStrings();
+		// Add path
+		LibRegistry libRegistry = LibRegistry.instance();
+		String cmd = ".libPaths(c(\"" + libRegistry.getInstallationPath().toString().replace("\\", "/")
+				+ "\", .libPaths()))";
+		String[] newPaths = controller.eval(cmd, true).asStrings();
 
-    // Run model
-    controller.eval(fskObj.param + "\n" + fskObj.model, false);
+		// Run model
+		controller.eval(fskObj.param + "\n" + fskObj.model, false);
 
-    // Save workspace
-    if (fskObj.workspace == null) {
-      fskObj.workspace = FileUtil.createTempFile("workspace", ".R");
-    }
-    controller.eval("save.image('" + fskObj.workspace.getAbsolutePath().replace("\\", "/") + "')",
-        false);
+		// Save workspace
+		if (fskObj.workspace == null) {
+			fskObj.workspace = FileUtil.createTempFile("workspace", ".R");
+		}
+		controller.eval("save.image('" + fskObj.workspace.getAbsolutePath().replace("\\", "/") + "')", false);
 
-    // Creates chart into m_imageFile
-    try {
-      controller.eval("png(\"" + internalSettings.imageFile.getAbsolutePath().replace("\\", "/")
-          + "\", width=640, height=640, pointsize=12, bg=\"#ffffff\", res=\"NA\")", false);
-      controller.eval(fskObj.viz + "\n", false);
-      controller.eval("dev.off()", false);
-    } catch (RException e) {
-      LOGGER.warn("Visualization script failed");
-    }
+		// Creates chart into m_imageFile
+		try {
 
-    // Restore .libPaths() to the original library path which happens to be
-    // in the last position
-    controller.eval(".libPaths()[" + newPaths.length + "]", false);
+			// TODO: Initialize necessary R stuff to plot
+			if (Platform.isMac()) {
+				controller.eval("library(Cairo)", false);
+				controller.eval("options(device='png', bitmapType='cairo')", false);
+			} else {
+				controller.eval("options(device='png')", false);
+			}
 
-    return fskObj;
-  }
+			String pngCmd = "png('" + internalSettings.imageFile.getAbsolutePath()
+					+ "', width=640, height=640, pointsize=12, bg='#ffffff', res='NA')";
+			controller.eval(pngCmd, false);
+			controller.eval(fskObj.viz, false);
+			controller.eval("dev.off()", false);
+		} catch (RException e) {
+			LOGGER.warn("Visualization script failed");
+		}
 
-  Image getResultImage() {
-    return internalSettings.plot;
-  }
+		// Restore .libPaths() to the original library path which happens to be
+		// in the last position
+		controller.eval(".libPaths()[" + newPaths.length + "]", false);
+
+		return fskObj;
+	}
+
+	Image getResultImage() {
+		return internalSettings.plot;
+	}
 }
