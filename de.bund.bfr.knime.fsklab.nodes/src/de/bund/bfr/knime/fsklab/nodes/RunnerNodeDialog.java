24e498ea75475c49500253f407c47573ceede0aa
miguelalba <miguel.de-alba-aparicio@bfr.bund.de>
2018-03-14 15:32:08 +0100

    Update RunnerNodeDialog with preview of parameter script

--- de.bund.bfr.knime.fsklab.nodes/src/de/bund/bfr/knime/fsklab/nodes/RunnerNodeDialog.java
+++ de.bund.bfr.knime.fsklab.nodes/src/de/bund/bfr/knime/fsklab/nodes/RunnerNodeDialog.java
@@ -18,27 +18,30 @@
  */
 package de.bund.bfr.knime.fsklab.nodes;
 
+import java.awt.BorderLayout;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.util.Arrays;
 import java.util.List;
 import java.util.ResourceBundle;
+import javax.swing.BorderFactory;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JComboBox;
 import javax.swing.JComponent;
 import javax.swing.JSpinner;
 import javax.swing.JTextField;
 import javax.swing.SpinnerNumberModel;
-import org.apache.commons.lang3.StringUtils;
 import org.knime.core.node.DataAwareNodeDialogPane;
 import org.knime.core.node.InvalidSettingsException;
 import org.knime.core.node.NodeSettingsRO;
 import org.knime.core.node.NodeSettingsWO;
 import org.knime.core.node.NotConfigurableException;
 import org.knime.core.node.port.PortObject;
-import org.knime.core.node.port.PortObjectSpec;
 import de.bund.bfr.knime.fsklab.FskPortObject;
 import de.bund.bfr.knime.fsklab.FskSimulation;
 import de.bund.bfr.knime.fsklab.nodes.ui.FLabel;
 import de.bund.bfr.knime.fsklab.nodes.ui.FPanel;
+import de.bund.bfr.knime.fsklab.nodes.ui.ScriptPanel;
 import de.bund.bfr.knime.fsklab.nodes.ui.UIUtils;
 import de.bund.bfr.knime.fsklab.nodes.ui.UTF8Control;
 
@@ -50,7 +53,8 @@ public class RunnerNodeDialog extends DataAwareNodeDialogPane {
   private final SpinnerNumberModel heightModel;
   private final JTextField resolutionField;
   private final SpinnerNumberModel textSizeModel;
-  private final DefaultComboBoxModel<String> simulationModel;
+  private final DefaultComboBoxModel<FskSimulation> simulationModel;
+  private final ScriptPanel scriptPanel;
 
   public RunnerNodeDialog() {
     settings = new RunnerNodeSettings();
@@ -59,7 +63,10 @@ public class RunnerNodeDialog extends DataAwareNodeDialogPane {
     heightModel = new SpinnerNumberModel(settings.height, null, null, 1);
     resolutionField = new JTextField(settings.res);
     textSizeModel = new SpinnerNumberModel(settings.pointSize, null, null, 1);
-    simulationModel = new DefaultComboBoxModel<String>();
+    simulationModel = new DefaultComboBoxModel<>();
+
+    scriptPanel = new ScriptPanel("Preview", "", false);
+    scriptPanel.setBackground(UIUtils.WHITE);
 
     createUI();
   }
@@ -68,24 +75,40 @@ public class RunnerNodeDialog extends DataAwareNodeDialogPane {
 
     ResourceBundle bundle = ResourceBundle.getBundle("RunnerNodeBundle", new UTF8Control());
 
+    // Labels
     FLabel widthLabel = new FLabel(bundle.getString("width_label"));
     FLabel heightLabel = new FLabel(bundle.getString("height_label"));
     FLabel resolutionLabel = new FLabel(bundle.getString("res_label"));
     FLabel textSizeLabel = new FLabel(bundle.getString("textsize_label"));
     FLabel simulationLabel = new FLabel(bundle.getString("simulation_label"));
-    List<FLabel> labels =
-        Arrays.asList(widthLabel, heightLabel, resolutionLabel, textSizeLabel, simulationLabel);
+    List<FLabel> labels = Arrays.asList(widthLabel, heightLabel, resolutionLabel, textSizeLabel);
 
+    // Fields
     JSpinner widthSpinner = new JSpinner(widthModel);
     JSpinner heightSpinner = new JSpinner(heightModel);
     JSpinner textSizeSpinner = new JSpinner(textSizeModel);
     resolutionField.setColumns(5);
     resolutionField.setHorizontalAlignment(JTextField.RIGHT);
 
-    JComboBox<String> simulationField = new JComboBox<>(simulationModel);
+    JComboBox<FskSimulation> simulationField = new JComboBox<>(simulationModel);
     // right align simulationField
     ((JTextField) simulationField.getEditor().getEditorComponent())
         .setHorizontalAlignment(JTextField.RIGHT);
+
+    simulationField.addItemListener(new ItemListener() {
+
+      @Override
+      public void itemStateChanged(ItemEvent e) {
+        FskSimulation selectedSimulation = (FskSimulation) simulationModel.getSelectedItem();
+
+        // The selectedSimulation is null when the simulationModel is cleared.
+        String previewScript =
+            selectedSimulation == null ? "" : NodeUtils.buildParameterScript(selectedSimulation);
+        scriptPanel.setText(previewScript);
+        scriptPanel.revalidate();
+        scriptPanel.repaint();
+      }
+    });
 
     // Set tooltips
     widthSpinner.setToolTipText(bundle.getString("width_tooltip"));
@@ -94,42 +117,29 @@ public class RunnerNodeDialog extends DataAwareNodeDialogPane {
     textSizeSpinner.setToolTipText(bundle.getString("textsize_tooltip"));
     simulationField.setToolTipText(bundle.getString("simulation_tooltip"));
 
-    List<JComponent> fields = Arrays.asList(widthSpinner, heightSpinner, resolutionField,
-        textSizeSpinner, simulationField);
+    List<JComponent> fields =
+        Arrays.asList(widthSpinner, heightSpinner, resolutionField, textSizeSpinner);
 
     FPanel formPanel = UIUtils.createFormPanel(labels, fields);
     FPanel northPanel = UIUtils.createNorthPanel(formPanel);
 
-    addTab("Options", northPanel);
-  }
+    // Simulation panel
+    {
+      FPanel simulationSelectionPanel =
+          UIUtils.createFormPanel(Arrays.asList(simulationLabel), Arrays.asList(simulationField));
+      scriptPanel.setBorder(BorderFactory.createTitledBorder("Preview"));
 
-  /** Load settings from saved settings. */
-  @Override
-  protected void loadSettingsFrom(NodeSettingsRO settings, PortObjectSpec[] specs)
-      throws NotConfigurableException {
+      FPanel simulationPanel = new FPanel();
+      simulationPanel.setBorder(BorderFactory.createTitledBorder("Simulation"));
 
-    try {
-      this.settings.load(settings);
+      simulationPanel.setLayout(new BorderLayout());
+      simulationPanel.add(simulationSelectionPanel, BorderLayout.NORTH);
+      simulationPanel.add(scriptPanel, BorderLayout.CENTER);
 
-      widthModel.setValue(this.settings.width);
-      heightModel.setValue(this.settings.height);
-      resolutionField.setText(this.settings.res);
-      textSizeModel.setValue(this.settings.pointSize);
-
-      // Remove selected simulation if not contained in new list of simulations. Otherwise set value
-      // from settings as selected.
-      boolean isContained = false;
-      for (int i = 0; i < simulationModel.getSize(); i++) {
-        if (this.settings.simulation.equals(simulationModel.getElementAt(i))) {
-          isContained = true;
-          break;
-        }
-      }
-      simulationModel.setSelectedItem(isContained ? this.settings.simulation : null);
-
-    } catch (InvalidSettingsException exception) {
-      throw new NotConfigurableException(exception.getMessage(), exception);
+      northPanel.add(simulationPanel);
     }
+
+    addTab("Options", northPanel);
   }
 
   /** Load settings from input ports. */
@@ -141,20 +151,28 @@ public class RunnerNodeDialog extends DataAwareNodeDialogPane {
 
     if (inputs.length == 1 && inputs[0] != null) {
       FskPortObject inObj = (FskPortObject) inputs[0];
-      for (FskSimulation fskSimulation : inObj.simulations) {
-        simulationModel.addElement(fskSimulation.getName());
-      }
 
-      // Remove selected simulation if not contained in new list of simulations. Otherwise set value
-      // from settings as selected.
-      boolean isContained = false;
+      // Update combobox
+      inObj.simulations.forEach(simulationModel::addElement);
+
+      // Look for simulation with the name stored in settings. If the simulation is not found
+      // (not contained) in new list of simulations then there is no selection.
+      FskSimulation selectedSimulation = null;
       for (int i = 0; i < simulationModel.getSize(); i++) {
-        if (this.settings.simulation.equals(simulationModel.getElementAt(i))) {
-          isContained = true;
+        FskSimulation currentSimulation = simulationModel.getElementAt(i);
+        if (currentSimulation.getName().equals(this.settings.simulation)) {
+          selectedSimulation = currentSimulation;
           break;
         }
       }
-      simulationModel.setSelectedItem(isContained ? this.settings.simulation : null);
+
+      // If there is no selection the default simulation is selected (first simulation)
+      if (selectedSimulation != null) {
+        simulationModel.setSelectedItem(selectedSimulation);
+      } else {
+        FskSimulation defaultSimulation = inObj.simulations.get(0);
+        simulationModel.setSelectedItem(defaultSimulation);
+      }
     }
   }
 
@@ -167,10 +185,8 @@ public class RunnerNodeDialog extends DataAwareNodeDialogPane {
     this.settings.pointSize = textSizeModel.getNumber().intValue();
 
     // selectedSimulation may be null if there is no selection
-    String selectedSimulation = (String) simulationModel.getSelectedItem();
-
-    // this.settings.simulation is set "" if selectedSimulation is null
-    this.settings.simulation = StringUtils.defaultString(selectedSimulation);
+    FskSimulation selectedSimulation = (FskSimulation) simulationModel.getSelectedItem();
+    this.settings.simulation = selectedSimulation.getName();
 
     this.settings.save(settings);
   }
