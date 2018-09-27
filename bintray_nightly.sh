2dcc850c2586bad44623de982cfa2b8a4255b5c9
Miguel <miguel.de-alba-aparicio@bfr.bund.de>
2018-09-27 14:04:59 +0200

    Changes nightly build to deploy to fsklab_test

--- bintray_nightly.sh
+++ bintray_nightly.sh
@@ -1,12 +1,12 @@
-API="https://api.bintray.com/content/silebat/nightly"
+API="https://api.bintray.com/content/silebat/fsklab_test"
 PROPS=";bt_package=update;bt_version=version;publish=1;override=1"
 FOLDER="$TRAVIS_BUILD_DIR/releng/de.bund.bfr.knime.update/target/repository"
 
 # Delete previous version 'version'
-curl -u $BINTRAY_USER:$BINTRAY_KEY -X DELETE https://api.bintray.com/packages/silebat/nightly/update/versions/version
+curl -u $BINTRAY_USER:$BINTRAY_KEY -X DELETE https://api.bintray.com/packages/silebat/fsklab_test/update/versions/version
 
 # Create version 'version'
-curl -u $BINTRAY_USER:$BINTRAY_KEY -H "Content-Type: application/json" -X POST -d '{"name": "version", "description": ""}' https://api.bintray.com/packages/silebat/nightly/update/versions
+curl -u $BINTRAY_USER:$BINTRAY_KEY -H "Content-Type: application/json" -X POST -d '{"name": "version", "description": ""}' https://api.bintray.com/packages/silebat/fsklab_test/update/versions
 
 # Upload artifacts and content
 curl -u $BINTRAY_USER:$BINTRAY_KEY -T $FOLDER/artifacts.jar $API/artifacts.jar$PROPS
