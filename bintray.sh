18d58a4ef4050105ef6994a1b8e49e683771e5be
Miguel <miguel.de-alba-aparicio@bfr.bund.de>
2018-10-12 18:41:46 +0200

    Update deployment script to update old FSK-Lab update site

--- bintray.sh
+++ bintray.sh
@@ -1,25 +1,50 @@
-API="https://api.bintray.com/content/silebat/fsklab_icpmf"
+API="https://api.bintray.com"
 PROPS=";bt_package=update;bt_version=version;publish=1;override=1"
 FOLDER="$TRAVIS_BUILD_DIR/releng/de.bund.bfr.knime.update/target/repository"
 
+# DEPLOY TO ICPMF repo
+# =====================================================================================================
 # Delete previous version 'version'
-curl -u $BINTRAY_USER:$BINTRAY_KEY -X DELETE https://api.bintray.com/packages/silebat/fsklab_icpmf/update/versions/version
+curl -u $BINTRAY_USER:$BINTRAY_KEY -X DELETE $API/content/silebat/fsklab_icpmf/update/versions/version
 
 # Create version 'version'
-curl -u $BINTRAY_USER:$BINTRAY_KEY -H "Content-Type: application/json" -X POST -d '{"name": "version", "description": ""}' https://api.bintray.com/packages/silebat/fsklab_icpmf/update/versions
+curl -u $BINTRAY_USER:$BINTRAY_KEY -H "Content-Type: application/json" -X POST -d '{"name": "version", "description": ""}' $API/packages/silebat/fsklab_icpmf/update/versions
 
 # Upload artifacts and content
-curl -u $BINTRAY_USER:$BINTRAY_KEY -T $FOLDER/artifacts.jar $API/artifacts.jar$PROPS
-curl -u $BINTRAY_USER:$BINTRAY_KEY -T $FOLDER/content.jar $API/content.jar$PROPS
+curl -u $BINTRAY_USER:$BINTRAY_KEY -T $FOLDER/artifacts.jar $API/content/silebat/fsklab_icpmf/artifacts.jar$PROPS
+curl -u $BINTRAY_USER:$BINTRAY_KEY -T $FOLDER/content.jar $API/content/silebat/fsklab_icpmf/content.jar$PROPS
 
 # Upload features (feature includes the features/ folder)
 for feature in $FOLDER/features/*.jar; do
 	file=$(basename -- "$feature")
-	curl -u $BINTRAY_USER:$BINTRAY_KEY -T $feature $API/features/$file$PROPS
+	curl -u $BINTRAY_USER:$BINTRAY_KEY -T $feature $API/content/silebat/fsklab_icpmf/features/$file$PROPS
 done
 
 # Upload plugins (plugin includes the plugins/ folder)
 for plugin in $FOLDER/plugins/*.jar; do
 	file=$(basename -- "$plugin")
-	curl -u $BINTRAY_USER:$BINTRAY_KEY -T $plugin $API/plugins/$file$PROPS
+	curl -u $BINTRAY_USER:$BINTRAY_KEY -T $plugin $API/content/silebat/fsklab_icpmf/plugins/$file$PROPS
+done
+
+# DEPLOY TO FSK-Lab repo
+# =====================================================================================================
+curl -u $BINTRAY_USER:$BINTRAY_KEY -X DELETE $API/content/silebat/fsklab/stable/versions/version
+
+# Create version 'version'
+curl -u $BINTRAY_USER:$BINTRAY_KEY -H "Content-Type: application/json" -X POST -d '{"name": "version", "description": ""}' $API/packages/silebat/fsklab/stable/versions
+
+# Upload artifacts and content
+curl -u $BINTRAY_USER:$BINTRAY_KEY -T $FOLDER/artifacts.jar $API/content/silebat/fsklab/artifacts.jar$PROPS
+curl -u $BINTRAY_USER:$BINTRAY_KEY -T $FOLDER/content.jar $API/content/silebat/fsklab/content.jar$PROPS
+
+# Upload features (feature includes the features/ folder)
+for feature in $FOLDER/features/*.jar; do
+	file=$(basename -- "$feature")
+	curl -u $BINTRAY_USER:$BINTRAY_KEY -T $feature $API/content/silebat/fsklab/features/$file$PROPS
+done
+
+# Upload plugins (plugin includes the plugins/ folder)
+for plugin in $FOLDER/plugins/*.jar; do
+	file=$(basename -- "$plugin")
+	curl -u $BINTRAY_USER:$BINTRAY_KEY -T $plugin $API/content/silebat/fsklab/plugins/$file$PROPS
 done
