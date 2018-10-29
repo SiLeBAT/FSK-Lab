384438a3de9ac1a1fbc184cbd6a5613b7310a977
Miguel <miguel.de-alba-aparicio@bfr.bund.de>
2018-10-29 11:24:55 +0100

    [refactor] MDLiteratureItem: add test, document methods and remove keys

---
+++ de.bund.bfr.knime.pmm.common.test/src/de/bund/bfr/knime/pmm/extendedtable/items/MDLiteratureItemTest.java
@@ -0,0 +1,112 @@
+package de.bund.bfr.knime.pmm.extendedtable.items;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import org.jdom2.Element;
+import org.junit.Test;
+
+public class MDLiteratureItemTest {
+
+	@Test
+	public void testConstructors() {
+
+		// Test fully parameterized constructor
+		MDLiteratureItem item0 = new MDLiteratureItem("author", 0, "title", "abstractText", "journal", "volume", "issue", 0,
+				0, "website", 0, "comment", 0, "dbuuid");
+		assertTrue(0 == item0.getId());
+		assertEquals("author", item0.getAuthor());
+		assertTrue(0 == item0.getYear());
+		assertEquals("journal", item0.getJournal());
+		assertEquals("volume", item0.getVolume());
+		assertEquals("issue", item0.getIssue());
+		assertTrue(0 == item0.getPage());
+		assertTrue(0 == item0.getApprovalMode());
+		assertEquals("website", item0.getWebsite());
+		assertTrue(0 == item0.getType());
+		assertEquals("comment", item0.getComment());
+		assertEquals("dbuuid", item0.getDbuuid());
+
+		// Test constructor without dbbuid
+		MDLiteratureItem item1 = new MDLiteratureItem("author", 0, "title", "abstractText", "journal", "volume", "issue", 0,
+				0, "website", 0, "comment", 0);
+		assertTrue(0 == item1.getId());
+		assertEquals("author", item1.getAuthor());
+		assertTrue(0 == item1.getYear());
+		assertEquals("journal", item1.getJournal());
+		assertEquals("volume", item1.getVolume());
+		assertEquals("issue", item1.getIssue());
+		assertTrue(0 == item1.getPage());
+		assertTrue(0 == item1.getApprovalMode());
+		assertEquals("website", item1.getWebsite());
+		assertTrue(0 == item1.getType());
+		assertEquals("comment", item1.getComment());
+		assertNull(item1.getDbuuid());
+
+		// Test constructor without dbbuid and id
+		MDLiteratureItem item2 = new MDLiteratureItem("author", 0, "title", "abstractText", "journal", "volume", "issue", 0,
+				0, "website", 0, "comment");
+		assertTrue(item2.getId() < 0);
+		assertEquals("author", item2.getAuthor());
+		assertTrue(0 == item2.getYear());
+		assertEquals("journal", item2.getJournal());
+		assertEquals("volume", item2.getVolume());
+		assertEquals("issue", item2.getIssue());
+		assertTrue(0 == item2.getPage());
+		assertTrue(0 == item2.getApprovalMode());
+		assertEquals("website", item2.getWebsite());
+		assertTrue(0 == item2.getType());
+		assertEquals("comment", item2.getComment());
+		assertNull(item2.getDbuuid());
+		
+		// Test copy constructor with Element
+		Element element = new Element(MDLiteratureItem.ELEMENT_LITERATURE);
+		element.setAttribute("id", "0");
+		element.setAttribute("author", "author");
+		element.setAttribute("year", "0");
+		element.setAttribute("journal", "journal");
+		element.setAttribute("volume", "volume");
+		element.setAttribute("issue", "issue");
+		element.setAttribute("page", "0");
+		element.setAttribute("approvalMode", "0");
+		element.setAttribute("website", "website");
+		element.setAttribute("type", "0");
+		element.setAttribute("comment", "comment");
+		element.setAttribute("dbuuid", "dbuuid");
+		
+		MDLiteratureItem item3 = new MDLiteratureItem(element);
+		assertTrue(0 == item3.getId());
+		assertEquals("author", item3.getAuthor());
+		assertTrue(0 == item3.getYear());
+		assertEquals("journal", item3.getJournal());
+		assertEquals("volume", item3.getVolume());
+		assertEquals("issue", item3.getIssue());
+		assertTrue(0 == item3.getPage());
+		assertTrue(0 == item3.getApprovalMode());
+		assertEquals("website", item3.getWebsite());
+		assertTrue(0 == item3.getType());
+		assertEquals("comment", item3.getComment());
+		assertEquals("dbuuid", item3.getDbuuid());
+	}
+
+	@Test
+	public void testToXmlElement() throws Exception {
+		MDLiteratureItem item = new MDLiteratureItem("author", 0, "title", "abstractText", "journal", "volume", "issue", 0,
+				0, "website", 0, "comment", 0, "dbuuid");
+		Element element = item.toXmlElement();
+		
+		assertTrue(0 == element.getAttribute("id").getIntValue());
+		assertEquals("author", element.getAttributeValue("author"));
+		assertTrue(0 == element.getAttribute("year").getIntValue());
+		assertEquals("journal", element.getAttributeValue("journal"));
+		assertEquals("volume", element.getAttributeValue("volume"));
+		assertEquals("issue", element.getAttributeValue("issue"));
+		assertTrue(0 == element.getAttribute("page").getIntValue());
+		assertTrue(0 == element.getAttribute("approvalMode").getIntValue());
+		assertEquals("website", element.getAttributeValue("website"));
+		assertTrue(0 == element.getAttribute("type").getIntValue());
+		assertEquals("comment", element.getAttributeValue("comment"));
+		assertEquals("dbuuid", element.getAttributeValue("dbuuid"));
+	}
+}
--- de.bund.bfr.knime.pmm.common/src/de/bund/bfr/knime/pmm/extendedtable/items/MDLiteratureItem.java
+++ de.bund.bfr.knime.pmm.common/src/de/bund/bfr/knime/pmm/extendedtable/items/MDLiteratureItem.java
@@ -29,21 +29,6 @@ public class MDLiteratureItem implements LiteratureItemI, PmmXmlElementConvertab
 
 	public static final String ELEMENT_LITERATURE = "MDLiteratureItem";
 
-	private static final String ATT_AUTHOR = "author";
-	private static final String ATT_YEAR = "year";
-	private static final String ATT_TITLE = "title";
-	private static final String ATT_ABSTRACT = "abstract";
-	private static final String ATT_JOURNAL = "journal";
-	private static final String ATT_VOLUME = "volume";
-	private static final String ATT_ISSUE = "issue";
-	private static final String ATT_PAGE = "page";
-	private static final String ATT_APPROVAL_MODE = "approvalMode";
-	private static final String ATT_WEBSITE = "website";
-	private static final String ATT_TYPE = "type";
-	private static final String ATT_COMMENT = "comment";
-	private static final String ATT_ID = "id";
-	private static final String ATT_DBUUID = "dbuuid";
-
 	private Integer id;
 	private String author;
 	private String title;
@@ -59,6 +44,7 @@ public class MDLiteratureItem implements LiteratureItemI, PmmXmlElementConvertab
 	private String comment;
 	private String dbuuid;
 
+	/** Fully parameterized constructor. */
 	public MDLiteratureItem(final String author, final Integer year, final String title, final String abstractText,
 			final String journal, final String volume, final String issue, final Integer page,
 			final Integer approvalMode, final String website, final Integer type, final String comment,
@@ -80,6 +66,10 @@ public class MDLiteratureItem implements LiteratureItemI, PmmXmlElementConvertab
 		this.dbuuid = dbuuid;
 	}
 
+	/**
+	 * Constructor with author, year, title, abstractText, journal, volume, issue,
+	 * page, approvalMode, website, type, comment and id. dbuuid is null.
+	 */
 	public MDLiteratureItem(final String author, final Integer year, final String title, final String abstractText,
 			final String journal, final String volume, final String issue, final Integer page,
 			final Integer approvalMode, final String website, final Integer type, final String comment,
@@ -88,6 +78,14 @@ public class MDLiteratureItem implements LiteratureItemI, PmmXmlElementConvertab
 				null);
 	}
 
+	/**
+	 * Constructor with author, year, title, abstractText, journal, volume, issue,
+	 * page, approvalMode, website, type and comment.
+	 * <ul>
+	 * <li>id is given a random negative int.
+	 * <li>dbuuid is null.
+	 * </ul>
+	 */
 	public MDLiteratureItem(final String author, final Integer year, final String title, final String abstractText,
 			final String journal, final String volume, final String issue, final Integer page,
 			final Integer approvalMode, final String website, final Integer type, final String comment) {
@@ -95,13 +93,33 @@ public class MDLiteratureItem implements LiteratureItemI, PmmXmlElementConvertab
 				MathUtilities.getRandomNegativeInt(), null);
 	}
 
+	/**
+	 * Copy constructor. Take every property from a {@link org.jdom2.Element} with
+	 * properties:
+	 * <ul>
+	 * <li>String "author"
+	 * <li>Integer "year"
+	 * <li>String "title"
+	 * <li>String "abstract"
+	 * <li>String "journal"
+	 * <li>String "volume"
+	 * <li>String "issue"
+	 * <li>Integer "page"
+	 * <li>Integer "approvalMode"
+	 * <li>String "website"
+	 * <li>Integer "type"
+	 * <li>String "comment"
+	 * <li>Integer "id"
+	 * <li>String "dbuuid"
+	 * </ul>
+	 */
 	public MDLiteratureItem(final Element el) {
-		this(XmlHelper.getString(el, ATT_AUTHOR), XmlHelper.getInt(el, ATT_YEAR), XmlHelper.getString(el, ATT_TITLE),
-				XmlHelper.getString(el, ATT_ABSTRACT), XmlHelper.getString(el, ATT_JOURNAL),
-				XmlHelper.getString(el, ATT_VOLUME), XmlHelper.getString(el, ATT_ISSUE), XmlHelper.getInt(el, ATT_PAGE),
-				XmlHelper.getInt(el, ATT_APPROVAL_MODE), XmlHelper.getString(el, ATT_WEBSITE),
-				XmlHelper.getInt(el, ATT_TYPE), XmlHelper.getString(el, ATT_COMMENT), XmlHelper.getInt(el, ATT_ID),
-				XmlHelper.getString(el, ATT_DBUUID));
+		this(XmlHelper.getString(el, "author"), XmlHelper.getInt(el, "year"), XmlHelper.getString(el, "title"),
+				XmlHelper.getString(el, "abstract"), XmlHelper.getString(el, "journal"),
+				XmlHelper.getString(el, "volume"), XmlHelper.getString(el, "issue"), XmlHelper.getInt(el, "page"),
+				XmlHelper.getInt(el, "approvalMode"), XmlHelper.getString(el, "website"),
+				XmlHelper.getInt(el, "type"), XmlHelper.getString(el, "comment"), XmlHelper.getInt(el, "id"),
+				XmlHelper.getString(el, "dbuuid"));
 	}
 
 	public MDLiteratureItem(final LiteratureItemI lit) {
@@ -110,28 +128,46 @@ public class MDLiteratureItem implements LiteratureItemI, PmmXmlElementConvertab
 				lit.getId(), lit.getDbuuid());
 	}
 
-	
 	public String getElementName() {
 		return "MDLiteratureItem";
 	}
-	
+
+	/**
+	 * Generate a {@link org.jdom2.Element} with name "MDLiteratureItem" and properties:
+	 * <ul>
+	 * <li>String "author"
+	 * <li>Integer "year"
+	 * <li>String "title"
+	 * <li>String "abstract"
+	 * <li>String "journal"
+	 * <li>String "volume"
+	 * <li>String "issue"
+	 * <li>Integer "page"
+	 * <li>Integer "approvalMode"
+	 * <li>String "website"
+	 * <li>Integer "type"
+	 * <li>String "comment"
+	 * <li>Integer "id"
+	 * <li>String "dbuuid"
+	 * </ul>
+	 */
 	public Element toXmlElement() {
 		Element ret = new Element(ELEMENT_LITERATURE);
 
-		ret.setAttribute(ATT_AUTHOR, XmlHelper.getNonNull(author));
-		ret.setAttribute(ATT_YEAR, XmlHelper.getNonNull(year));
-		ret.setAttribute(ATT_TITLE, XmlHelper.removeDirt(title));
-		ret.setAttribute(ATT_ABSTRACT, XmlHelper.removeDirt(abstractText));
-		ret.setAttribute(ATT_JOURNAL, XmlHelper.getNonNull(journal));
-		ret.setAttribute(ATT_VOLUME, XmlHelper.getNonNull(volume));
-		ret.setAttribute(ATT_ISSUE, XmlHelper.getNonNull(issue));
-		ret.setAttribute(ATT_PAGE, XmlHelper.getNonNull(page));
-		ret.setAttribute(ATT_APPROVAL_MODE, XmlHelper.getNonNull(approvalMode));
-		ret.setAttribute(ATT_WEBSITE, XmlHelper.getNonNull(website));
-		ret.setAttribute(ATT_TYPE, XmlHelper.getNonNull(type));
-		ret.setAttribute(ATT_COMMENT, XmlHelper.getNonNull(comment));
-		ret.setAttribute(ATT_ID, XmlHelper.getNonNull(id));
-		ret.setAttribute(ATT_DBUUID, XmlHelper.getNonNull(dbuuid));
+		ret.setAttribute("author", XmlHelper.getNonNull(author));
+		ret.setAttribute("year", XmlHelper.getNonNull(year));
+		ret.setAttribute("title", XmlHelper.removeDirt(title));
+		ret.setAttribute("abstract", XmlHelper.removeDirt(abstractText));
+		ret.setAttribute("journal", XmlHelper.getNonNull(journal));
+		ret.setAttribute("volume", XmlHelper.getNonNull(volume));
+		ret.setAttribute("issue", XmlHelper.getNonNull(issue));
+		ret.setAttribute("page", XmlHelper.getNonNull(page));
+		ret.setAttribute("approvalMode", XmlHelper.getNonNull(approvalMode));
+		ret.setAttribute("website", XmlHelper.getNonNull(website));
+		ret.setAttribute("type", XmlHelper.getNonNull(type));
+		ret.setAttribute("comment", XmlHelper.getNonNull(comment));
+		ret.setAttribute("id", XmlHelper.getNonNull(id));
+		ret.setAttribute("dbuuid", XmlHelper.getNonNull(dbuuid));
 
 		return ret;
 	}
