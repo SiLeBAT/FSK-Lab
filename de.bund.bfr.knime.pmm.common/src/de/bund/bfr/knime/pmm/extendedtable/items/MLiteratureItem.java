a0c8c899f79d20759c898ea8c943371785573b7c
Miguel <miguel.de-alba-aparicio@bfr.bund.de>
2018-12-13 17:36:04 +0100

    Refactor: Change MMLiteratureItem to extend abstract class
    Remove duplicated properties, now moved to the abstract class
    LiteratureItem

--- de.bund.bfr.knime.pmm.common/src/de/bund/bfr/knime/pmm/extendedtable/items/MLiteratureItem.java
+++ de.bund.bfr.knime.pmm.common/src/de/bund/bfr/knime/pmm/extendedtable/items/MLiteratureItem.java
@@ -25,24 +25,9 @@ import de.bund.bfr.knime.pmm.common.PmmXmlElementConvertable;
 import de.bund.bfr.knime.pmm.common.XmlHelper;
 import de.bund.bfr.knime.pmm.common.math.MathUtilities;
 
-public class MLiteratureItem implements LiteratureItemI, PmmXmlElementConvertable {
+public class MLiteratureItem extends LiteratureItem implements PmmXmlElementConvertable {
 
 	public static final String ELEMENT_LITERATURE = "ModelLiterature";
-
-	private Integer id;
-	private String author;
-	private String title;
-	private String abstractText;
-	private Integer year;
-	private String journal;
-	private String volume;
-	private String issue;
-	private Integer page;
-	private Integer approvalMode;
-	private String website;
-	private Integer type;
-	private String comment;
-	private String dbuuid;
 
 	/** Fully parameterized constructor. */
 	public MLiteratureItem(final String author, final Integer year, final String title, final String abstractText,
@@ -117,9 +102,8 @@ public class MLiteratureItem implements LiteratureItemI, PmmXmlElementConvertabl
 		this(XmlHelper.getString(el, "author"), XmlHelper.getInt(el, "year"), XmlHelper.getString(el, "title"),
 				XmlHelper.getString(el, "abstract"), XmlHelper.getString(el, "journal"),
 				XmlHelper.getString(el, "volume"), XmlHelper.getString(el, "issue"), XmlHelper.getInt(el, "page"),
-				XmlHelper.getInt(el, "approvalMode"), XmlHelper.getString(el, "website"),
-				XmlHelper.getInt(el, "type"), XmlHelper.getString(el, "comment"), XmlHelper.getInt(el, "id"),
-				XmlHelper.getString(el, "dbuuid"));
+				XmlHelper.getInt(el, "approvalMode"), XmlHelper.getString(el, "website"), XmlHelper.getInt(el, "type"),
+				XmlHelper.getString(el, "comment"), XmlHelper.getInt(el, "id"), XmlHelper.getString(el, "dbuuid"));
 	}
 
 	/** Copy constructor. */
@@ -128,13 +112,14 @@ public class MLiteratureItem implements LiteratureItemI, PmmXmlElementConvertabl
 				lit.getIssue(), lit.getPage(), lit.getApprovalMode(), lit.getWebsite(), lit.getType(), lit.getComment(),
 				lit.getId(), lit.getDbuuid());
 	}
-	
+
 	public String getElementName() {
 		return "ModelLiterature";
 	}
 
 	/**
-	 * Generate a {@link org.jdom2.Element} with name "ModelLiterature" and properties:
+	 * Generate a {@link org.jdom2.Element} with name "ModelLiterature" and
+	 * properties:
 	 * <ul>
 	 * <li>String "author"
 	 * <li>Integer "year"
@@ -177,117 +162,5 @@ public class MLiteratureItem implements LiteratureItemI, PmmXmlElementConvertabl
 	@Override
 	public String toString() {
 		return author + "_" + year + "_" + title;
-	}
-
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getAuthor() {
-		return author;
-	}
-
-	public void setAuthor(String author) {
-		this.author = author;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getAbstractText() {
-		return abstractText;
-	}
-
-	public void setAbstractText(String abstractText) {
-		this.abstractText = abstractText;
-	}
-
-	public Integer getYear() {
-		return year;
-	}
-
-	public void setYear(Integer year) {
-		this.year = year;
-	}
-
-	public String getJournal() {
-		return journal;
-	}
-
-	public void setJournal(String journal) {
-		this.journal = journal;
-	}
-
-	public String getVolume() {
-		return volume;
-	}
-
-	public void setVolume(String volume) {
-		this.volume = volume;
-	}
-
-	public String getIssue() {
-		return issue;
-	}
-
-	public void setIssue(String issue) {
-		this.issue = issue;
-	}
-
-	public Integer getPage() {
-		return page;
-	}
-
-	public void setPage(Integer page) {
-		this.page = page;
-	}
-
-	public Integer getApprovalMode() {
-		return approvalMode;
-	}
-
-	public void setApprovalMode(Integer approvalMode) {
-		this.approvalMode = approvalMode;
-	}
-
-	public String getWebsite() {
-		return website;
-	}
-
-	public void setWebsite(String website) {
-		this.website = website;
-	}
-
-	public Integer getType() {
-		return type;
-	}
-
-	public void setType(Integer type) {
-		this.type = type;
-	}
-
-	public String getComment() {
-		return comment;
-	}
-
-	public void setComment(String comment) {
-		this.comment = comment;
-	}
-
-	public String getDbuuid() {
-		return dbuuid;
-	}
-
-	public void setDbuuid(String dbuuid) {
-		this.dbuuid = dbuuid;
 	}
 }
