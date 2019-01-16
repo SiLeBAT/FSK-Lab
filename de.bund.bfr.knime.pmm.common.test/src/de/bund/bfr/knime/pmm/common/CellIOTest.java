74c78ba4ccbb0f196fad91f6b95f9a026bcd9e50
Miguel <miguel.de-alba-aparicio@bfr.bund.de>
2019-01-16 14:36:27 +0100

    Add more tests to CellIO
    - createIntCell(String): DataCell
    - createDoubleCell(String): DataCell
    - createMisssingCell(): DataCell
    - createXmlCell(PmmXmlDoc): DataCell
    - createXmlCell(TimeSeriesMetadata): DataCell
    - createXmlCell(Model1Metadata): DataCell
    - createXmlCell(Model2Metadata): DataCell

--- de.bund.bfr.knime.pmm.common.test/src/de/bund/bfr/knime/pmm/common/CellIOTest.java
+++ de.bund.bfr.knime.pmm.common.test/src/de/bund/bfr/knime/pmm/common/CellIOTest.java
@@ -10,9 +10,12 @@ import org.knime.core.data.DataType;
 import org.knime.core.data.def.DoubleCell;
 import org.knime.core.data.def.IntCell;
 import org.knime.core.data.def.StringCell;
+import org.knime.core.data.xml.XMLCell;
 import org.knime.core.data.xml.XMLCellFactory;
 
-import de.bund.bfr.knime.pmm.common.PmmXmlDoc;
+import de.bund.bfr.knime.pmm.extendedtable.Model1Metadata;
+import de.bund.bfr.knime.pmm.extendedtable.Model2Metadata;
+import de.bund.bfr.knime.pmm.extendedtable.TimeSeriesMetadata;
 
 public class CellIOTest {
 
@@ -60,8 +63,50 @@ public class CellIOTest {
 	}
 
 	@Test
-	public void testGetIntList() throws Exception {
+	public void testGetIntList() {
 		assertTrue(CellIO.getIntList(DataType.getMissingCell()).isEmpty());
 		assertEquals(Arrays.asList(1, 2, 3), CellIO.getIntList(new StringCell("1,2,3")));
+	}
+
+	@Test
+	public void testCreateIntCell() {
+		assertTrue(CellIO.createIntCell(null).isMissing());
+		assertEquals(1, ((IntCell) CellIO.createIntCell("1")).getIntValue());
+		assertTrue(CellIO.createIntCell("NAN").isMissing());
+	}
+
+	@Test
+	public void testCreateDoubleCell() {
+		assertTrue(CellIO.createDoubleCell(null).isMissing());
+		assertEquals(2.0, ((DoubleCell) CellIO.createDoubleCell("2.0")).getDoubleValue(), .0);
+		assertTrue(CellIO.createDoubleCell("NAN").isMissing());
+	}
+
+	@Test
+	public void testCreateXmlCell() {
+
+		// #createXMLCell(PmmXmlDoc)
+		assertNull(CellIO.createXmlCell((PmmXmlDoc) null));
+
+		XMLCell cell0 = (XMLCell) CellIO.createXmlCell(new PmmXmlDoc(new MatrixXml()));
+		assertEquals("matrix", cell0.getDocument().getFirstChild().getFirstChild().getNodeName());
+
+		// #createXMLCell(TimeSeriesMetadata)
+		assertNull(CellIO.createXmlCell((TimeSeriesMetadata) null));
+
+		XMLCell cell1 = (XMLCell) CellIO.createXmlCell(new TimeSeriesMetadata());
+		assertEquals("PmmDoc", cell1.getDocument().getFirstChild().getNodeName());
+
+		// #createXMLCell(Model1Metadata)
+		assertNull(CellIO.createXmlCell((Model1Metadata) null));
+
+		XMLCell cell2 = (XMLCell) CellIO.createXmlCell(new Model1Metadata());
+		assertEquals("PmmDoc", cell2.getDocument().getFirstChild().getNodeName());
+
+		// #createXMLCell(Model2Metadata)
+		assertNull(CellIO.createXmlCell((Model2Metadata) null));
+
+		XMLCell cell3 = (XMLCell) CellIO.createXmlCell(new Model2Metadata());
+		assertEquals("PmmDoc", cell3.getDocument().getFirstChild().getNodeName());
 	}
 }
