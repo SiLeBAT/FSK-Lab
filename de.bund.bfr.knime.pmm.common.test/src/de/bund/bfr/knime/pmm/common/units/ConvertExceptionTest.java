acc8cfc2a930ce3fcd6f9a650a3bd835f3c2c5bb
miguelalba <Miguel.de-Alba-Aparicio@bfr.bund.de>
2019-01-29 16:06:54 +0100

    Test ConvertException

---
+++ de.bund.bfr.knime.pmm.common.test/src/de/bund/bfr/knime/pmm/common/units/ConvertExceptionTest.java
@@ -0,0 +1,15 @@
+package de.bund.bfr.knime.pmm.common.units;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class ConvertExceptionTest {
+
+	@Test
+	public void test() {
+		ConvertException exception = new ConvertException("fromUnit", "toUnit");
+		assertEquals("fromUnit", exception.getFromUnit());
+		assertEquals("toUnit", exception.getToUnit());
+	}
+}
