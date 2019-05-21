200fdabc4b545413e1066c14f13b513d248fc34a
Miguel de Alba <Miguel.de-Alba-Aparicio@bfr.bund.de>
2019-05-21 13:41:32 +0200

    Restore deployment script and remove test deployment
    As the integration test is checked and finished the test deployment in the Travis configuration can be removed and the deployment script is enabled again.

--- bintray.sh
+++ bintray.sh
@@ -8,23 +8,23 @@ REPO=fsklab_test
 # DEPLOY TO test repo
 # =====================================================================================================
 # Delete previous version 'version'
-#curl -u $BINTRAY_USER:$BINTRAY_KEY -X DELETE $API/packages/$SUBJECT/$REPO/update/versions/version
+curl -u $BINTRAY_USER:$BINTRAY_KEY -X DELETE $API/packages/$SUBJECT/$REPO/update/versions/version
 
 # Create version 'version'
-#curl -u $BINTRAY_USER:$BINTRAY_KEY -H "Content-Type: application/json" -X POST -d '{"name": "version", "description": ""}' $API/packages/$SUBJECT/$REPO/update/versions
+curl -u $BINTRAY_USER:$BINTRAY_KEY -H "Content-Type: application/json" -X POST -d '{"name": "version", "description": ""}' $API/packages/$SUBJECT/$REPO/update/versions
 
 # Upload artifacts and content
-#curl -u $BINTRAY_USER:$BINTRAY_KEY -T $FOLDER/artifacts.jar $API/content/$SUBJECT/$REPO/artifacts.jar$PROPS
-#curl -u $BINTRAY_USER:$BINTRAY_KEY -T $FOLDER/content.jar $API/content/$SUBJECT/$REPO/content.jar$PROPS
+curl -u $BINTRAY_USER:$BINTRAY_KEY -T $FOLDER/artifacts.jar $API/content/$SUBJECT/$REPO/artifacts.jar$PROPS
+curl -u $BINTRAY_USER:$BINTRAY_KEY -T $FOLDER/content.jar $API/content/$SUBJECT/$REPO/content.jar$PROPS
 
 # Upload features (feature includes the features/ folder)
-#for feature in $FOLDER/features/*.jar; do
-#	file=$(basename -- "$feature")
-#	curl -u $BINTRAY_USER:$BINTRAY_KEY -T $feature $API/content/$SUBJECT/$REPO/features/$file$PROPS
-#done
+for feature in $FOLDER/features/*.jar; do
+	file=$(basename -- "$feature")
+	curl -u $BINTRAY_USER:$BINTRAY_KEY -T $feature $API/content/$SUBJECT/$REPO/features/$file$PROPS
+done
 
 # Upload plugins (plugin includes the plugins/ folder)
-#for plugin in $FOLDER/plugins/*.jar; do
-#	file=$(basename -- "$plugin")
-#	curl -u $BINTRAY_USER:$BINTRAY_KEY -T $plugin $API/content/$SUBJECT/$REPO/plugins/$file$PROPS
-#done
+for plugin in $FOLDER/plugins/*.jar; do
+	file=$(basename -- "$plugin")
+	curl -u $BINTRAY_USER:$BINTRAY_KEY -T $plugin $API/content/$SUBJECT/$REPO/plugins/$file$PROPS
+done
