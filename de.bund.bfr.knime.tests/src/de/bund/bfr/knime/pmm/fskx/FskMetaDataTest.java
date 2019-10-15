afaba996b993f0a102fac27d1815234592ffb299
Miguel de Alba <miguelalba89@gmail.com>
2019-10-15 15:37:21 +0200

    Refactor FskMetaDataTest with Hamcrest

--- de.bund.bfr.knime.tests/src/de/bund/bfr/knime/pmm/fskx/FskMetaDataTest.java
+++ de.bund.bfr.knime.tests/src/de/bund/bfr/knime/pmm/fskx/FskMetaDataTest.java
@@ -1,9 +1,10 @@
 package de.bund.bfr.knime.pmm.fskx;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+import static org.hamcrest.collection.IsEmptyCollection.empty;
 
 import org.junit.Test;
 
@@ -17,30 +18,30 @@ public class FskMetaDataTest {
 
 		final FskMetaData metaData = new FskMetaData();
 
-		assertNull(metaData.modelName);
-		assertNull(metaData.modelId);
-		assertNull(metaData.modelLink);
-		assertNull(metaData.organism);
-		assertNull(metaData.organismDetails);
-		assertNull(metaData.matrix);
-		assertNull(metaData.matrixDetails);
-		assertNull(metaData.creator);
-		assertNull(metaData.familyName);
-		assertNull(metaData.contact);
-		assertNull(metaData.software);
-		assertNull(metaData.referenceDescription);
-		assertNull(metaData.referenceDescriptionLink);
-		assertNull(metaData.createdDate);
-		assertNull(metaData.modifiedDate);
-		assertNull(metaData.rights);
-		assertNull(metaData.notes);
-		assertFalse(metaData.curated);
-		assertNull(metaData.type);
-		assertEquals(ModelClass.UNKNOWN, metaData.subject);
-		assertNull(metaData.foodProcess);
+		assertThat(metaData.modelName, is(nullValue()));		
+		assertThat(metaData.modelId, is(nullValue()));
+		assertThat(metaData.modelLink, is(nullValue()));
+		assertThat(metaData.organism, is(nullValue()));
+		assertThat(metaData.organismDetails, is(nullValue()));
+		assertThat(metaData.matrix, is(nullValue()));
+		assertThat(metaData.matrixDetails, is(nullValue()));
+		assertThat(metaData.creator, is(nullValue()));
+		assertThat(metaData.familyName, is(nullValue()));
+		assertThat(metaData.contact, is(nullValue()));
+		assertThat(metaData.software, is(nullValue()));
+		assertThat(metaData.referenceDescription, is(nullValue()));
+		assertThat(metaData.referenceDescriptionLink, is(nullValue()));
+		assertThat(metaData.createdDate, is(nullValue()));
+		assertThat(metaData.modifiedDate, is(nullValue()));
+		assertThat(metaData.rights, is(nullValue()));
+		assertThat(metaData.notes, is(nullValue()));
+		assertThat(metaData.curated, is(false));
+		assertThat(metaData.type, is(nullValue()));
+		assertThat(metaData.subject, equalTo(ModelClass.UNKNOWN));
+		assertThat(metaData.foodProcess, is(nullValue()));
 		// dependentVariable
-		assertTrue(metaData.independentVariables.isEmpty());
-		assertFalse(metaData.hasData);
+		assertThat(metaData.independentVariables, empty());
+		assertThat(metaData.hasData, is(false));
 	}
 
 }
