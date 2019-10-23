ea8975b2bd2b763bfb0a267abc77cecc414d64b9
Miguel <miguelalba89@gmail.com>
2019-10-23 14:04:37 +0200

    Add general information panel

---
+++ de.bund.bfr.knime.fsklab.nodes/js-src/de/bund/bfr/knime/fsklab/nodes/miguel_editor_data.js
@@ -0,0 +1,18 @@
+const descriptions = {
+    "generalInformation": {
+        "name": "A name given to the model or data",
+        "source": "A source from which the model/data is derived",
+        "identifier": "An unambiguous ID given to the model or data. This can also be created automatically by a software tool",
+        "creationDate": "Creation date/time of the FSK file",
+        "rights": "Rights granted for usage, distribution and modification of this FSK file",
+        "availability": "Availability of data or model, i.e. if the annotated model code / data is included in this FSK file",
+        "url": "Web address referencing the resource location (data for example)",
+        "format": "File extension of the model or data file (including version number of format if applicable)",
+        "language": "A language of the resource (some data or reports can be available in French language for example)",
+        "software": "The program or software language in which the model has been implemented",
+        "languageWrittenIn": "Software language used to write the model, e.g. R or MatLab",
+        "status": "The curation status of the model",
+        "objective": "Objective of the model or data",
+        "description": "General description of the study, data or model"
+    }
+}
