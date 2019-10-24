9473a530e083be674c22330276668495543c0eba
Miguel <miguelalba89@gmail.com>
2019-10-24 16:43:02 +0200

    Add up to contact

--- de.bund.bfr.knime.fsklab.nodes/js-src/de/bund/bfr/knime/fsklab/nodes/miguel_editor_data.js
+++ de.bund.bfr.knime.fsklab.nodes/js-src/de/bund/bfr/knime/fsklab/nodes/miguel_editor_data.js
@@ -1,18 +1,86 @@
-const descriptions = {
-    "generalInformation": {
-        "name": "A name given to the model or data",
-        "source": "A source from which the model/data is derived",
-        "identifier": "An unambiguous ID given to the model or data. This can also be created automatically by a software tool",
-        "creationDate": "Creation date/time of the FSK file",
-        "rights": "Rights granted for usage, distribution and modification of this FSK file",
-        "availability": "Availability of data or model, i.e. if the annotated model code / data is included in this FSK file",
-        "url": "Web address referencing the resource location (data for example)",
-        "format": "File extension of the model or data file (including version number of format if applicable)",
-        "language": "A language of the resource (some data or reports can be available in French language for example)",
-        "software": "The program or software language in which the model has been implemented",
-        "languageWrittenIn": "Software language used to write the model, e.g. R or MatLab",
-        "status": "The curation status of the model",
-        "objective": "Objective of the model or data",
-        "description": "General description of the study, data or model"
-    }
+const ui = {
+    "generalInformation": [{
+            "id": "name",
+            "label": "Name",
+            "type": "text",
+            "description": "A name given to the model or data"
+        }, {
+            "id": "source",
+            "label": "Source",
+            "type": "text",
+            "description": "A source from which the model/data is derived"
+        }, {
+            "id": "identifier",
+            "label": "Identifier",
+            "type": "text",
+            "description": "An unambiguous ID given to the model or data. This can also be created automatically by a software tool"
+        }, {
+            "id": "creationDate",
+            "label": "Creation date",
+            "type": "date",
+            "description": "Creation date/time of the FSK file"
+        }, {
+            "id": "rights",
+            "label": "Rights",
+            "type": "text",
+            "description": "Rights granted for usage, distribution and modification of this FSK file"
+        }, {
+            "id": "availability",
+            "label": "Availability",
+            "type": "boolean",
+            "description": "Availability of data or model, i.e. if the annotated model code / data is included in this FSK file"
+        }, {
+            "id": "url",
+            "label": "URL",
+            "type": "url",
+            "description": "Web address referencing the resource location (data for example)"
+        }, {
+            "id": "format",
+            "label": "Format",
+            "type": "text",
+            "description": "File extension of the model or data file (including version number of format if applicable)"
+        }, {
+            "id": "language",
+            "label": "Language",
+            "type": "text",
+            "description": "A language of the resource (some data or reports can be available in French language for example)"
+        }, {
+            "id": "software",
+            "label": "Software",
+            "type": "text",
+            "description": "The program or software language in which the model has been implemented"
+        }, {
+            "id": "languageWrittenIn",
+            "label": "Language written in",
+            "type": "text",
+            "description": "Software language used to write the model, e.g. R or MatLab"
+        }, {
+            "id": "status",
+            "label": "Status",
+            "type": "text",
+            "description": "The curation status of the model"
+        }, {
+            "id": "objective",
+            "label": "Objective",
+            "type": "text",
+            "description": "Objective of the model or data"
+        }, {
+            "id": "description",
+            "label": "Description",
+            "type": "text",
+            "description": "General description of the study, data or model"
+        }
+    ],
+    "modelCategory": [{
+            "id": "modelClass",
+            "label": "Model class",
+            "type": "text",
+            "description": "Type of model according to RAKIP classification"
+        }, {
+            "id": "modelClassComment",
+            "label": "Comment",
+            "type": "text",
+            "description": ""
+        }
+    ]
 }
