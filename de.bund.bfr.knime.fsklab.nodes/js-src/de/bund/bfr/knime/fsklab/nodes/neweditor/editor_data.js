0d56ba4fe4fe78d9f44d561c6239a142e0e6ad53
Miguel de Alba <miguel@Miguels-MacBook-Air.local>
2019-11-19 13:09:19 +0100

    Add Dose response model to UI schema

--- de.bund.bfr.knime.fsklab.nodes/js-src/de/bund/bfr/knime/fsklab/nodes/neweditor/editor_data.js
+++ de.bund.bfr.knime.fsklab.nodes/js-src/de/bund/bfr/knime/fsklab/nodes/neweditor/editor_data.js
@@ -510,7 +510,7 @@ const ui = {
         "label": "Technology type",
         "type": "text",
         "description": "The technology being employed to observe this measurement",
-        "vocabulary": "Study_Assay_Technology_Type"        
+        "vocabulary": "Study_Assay_Technology_Type"
     }, {
         "id": "assayTechnologyPlatform",
         "label": "Technology platform",
@@ -831,27 +831,27 @@ const ui = {
         "id": "mse",
         "label": "MSE",
         "type": "number",
-        "description": "Statistical values calculated to describe the performance of the model fitting procedure"        
+        "description": "Statistical values calculated to describe the performance of the model fitting procedure"
     }, {
         "id": "rmse",
         "label": "RMSE",
         "type": "number",
-        "description": "Statistical values calculated to describe the performance of the model fitting procedure"        
+        "description": "Statistical values calculated to describe the performance of the model fitting procedure"
     }, {
         "id": "rsquared",
         "label": "R squared",
         "type": "number",
-        "description": "Statistical values calculated to describe the performance of the model fitting procedure"        
+        "description": "Statistical values calculated to describe the performance of the model fitting procedure"
     }, {
         "id": "aic",
         "label": "AIC",
         "type": "number",
-        "description": "Statistical values calculated to describe the performance of the model fitting procedure"        
+        "description": "Statistical values calculated to describe the performance of the model fitting procedure"
     }, {
         "id": "bic",
         "label": "BIC",
         "type": "number",
-        "description": "Statistical values calculated to describe the performance of the model fitting procedure"        
+        "description": "Statistical values calculated to describe the performance of the model fitting procedure"
     }, {
         "id": "sensitivityAnalysis",
         "label": "Sensitivity analysis",
@@ -910,4 +910,318 @@ const ui = {
         "type": "text",
         "description": "Analysis to estimate uncertainty"
     }]
+}
+
+// TODO: Predictive model's data background
+const predictiveModelDataBackground = {
+
+}
+
+const schemas = {
+
+    genericModel: {
+        // general information
+        generalInformation: [
+            {
+                "id": "name",
+                "label": "Name",
+                "type": "text",
+                "description": "A name given to the model or data",
+                "required": true
+            }, {
+                "id": "source",
+                "label": "Source",
+                "type": "text",
+                "description": "A source from which the model/data is derived",
+                "vocabulary": "Source"
+            }, {
+                "id": "identifier",
+                "label": "Identifier",
+                "type": "text",
+                "description": "An unambiguous ID given to the model or data. This can also be created automatically by a software tool",
+                "required": true
+            }, {
+                "id": "creationDate",
+                "label": "Creation date",
+                "type": "date",
+                "description": "Creation date/time of the FSK file",
+                "required": true
+            }, {
+                "id": "modificationDate",
+                "label": "Modification date",
+                "type": "date-array",
+                "description": "Date/time of the last version of the FSK file"
+            }, {
+                "id": "rights",
+                "label": "Rights",
+                "type": "text",
+                "description": "Rights granted for usage, distribution and modification of this FSK file",
+                "vocabulary": "Rights",
+                "required": true
+            }, {
+                "id": "availability",
+                "label": "Availability",
+                "type": "text",
+                "description": "Availability of data or model, i.e. if the annotated model code / data is included in this FSK file",
+                "vocabulary": "Availability"
+            }, {
+                "id": "url",
+                "label": "URL",
+                "type": "url",
+                "description": "Web address referencing the resource location (data for example)"
+            }, {
+                "id": "format",
+                "label": "Format",
+                "type": "text",
+                "description": "File extension of the model or data file (including version number of format if applicable)",
+                "vocabulary": "Format"
+            }, {
+                "id": "language",
+                "label": "Language",
+                "type": "text",
+                "description": "A language of the resource (some data or reports can be available in French language for example)",
+                "vocabulary": "Language"
+            }, {
+                "id": "software",
+                "label": "Software",
+                "type": "text",
+                "description": "The program or software language in which the model has been implemented",
+                "vocabulary": "Software"
+            }, {
+                "id": "languageWrittenIn",
+                "label": "Language written in",
+                "type": "text",
+                "description": "Software language used to write the model, e.g. R or MatLab",
+                "vocabulary": "Language_written_in"
+            }, {
+                "id": "status",
+                "label": "Status",
+                "type": "text",
+                "description": "The curation status of the model",
+                "vocabulary": "Status"
+            }, {
+                "id": "objective",
+                "label": "Objective",
+                "type": "text",
+                "description": "Objective of the model or data"
+            }, {
+                "id": "description",
+                "label": "Description",
+                "type": "text",
+                "description": "General description of the study, data or model"
+            }
+        ],
+        contact: ui.contact,
+        reference: ui.reference,
+
+        // scope
+        scope: [
+            {
+                id: "generalComment",
+                label: "General comment",
+                type: "text",
+                description: "General comments on the scope of the study, data or model"
+            },
+            {
+                id: "temporalInformation",
+                label: "Temporal information",
+                type: "text",
+                description: "Spatial information (area) on which the model or data applies"
+            },
+            {
+                id: "spatialInformation",
+                label: "Spatial information",
+                type: "text",
+                description: "" // TODO: spatial information description
+            }
+        ],
+        product: ui.product,
+        hazard: ui.hazard,
+        populationGroup: ui.populationGroup,
+
+        // data background
+        study: ui.study,
+        studySample: ui.studySample,
+        dietaryAssessmentMethod: ui.dietaryAssessmentMethod,
+        laboratory: ui.laboratory,
+        assay: ui.assay,
+
+        // model math
+        modelMath: [
+            {
+                id: "fittingProcedure",
+                label: "Fitting procedure",
+                type: "text",
+                description: "Procedure used to fit the data to the model equation"
+            }, {
+                id: "event",
+                label: "Event",
+                type: "text-array",
+                description: "Definition of time-dependent parameter changes"
+            }
+        ],
+        parameter: ui.parameter,
+        qualityMeasures: ui.qualityMeasures,
+        modelEquation: ui.modelEquation,
+        exposure: ui.exposure,
+    },
+
+    doseResponseModel: {
+        generalInformation: [
+            {
+                id: "modelName",
+                label: "Model name",
+                type: "text",
+                description: "", // TODO: Model name description
+                required: true
+            },
+            {
+                id: "source",
+                label: "Source",
+                type: "text",
+                description: "", // TODO: source description
+                vocabulary: "Source"
+            },
+            {
+                id: "identifier",
+                label: "Identifier",
+                type: "text",
+                description: "", // TODO: identifier description
+                required: true
+            },
+            {
+                id: "creationDate",
+                label: "Creation date",
+                type: "date",
+                description: "", // TODO: creation date description
+                required: true
+            },
+            {
+                id: "modificationDate",
+                label: "Modification date",
+                type: "date-array",
+                description: "", // TODO: modification date description
+            },
+            {
+                id: "rights",
+                label: "Rights",
+                type: "text",
+                description: "", // TODO: rights description
+                vocabulary: "Rights",
+                required: true
+            },
+            {
+                id: "availability",
+                label: "Availability",
+                type: "text",
+                description: "", // TODO: availability description
+                vocabulary: "Availability"
+            },
+            {
+                id: "url",
+                label: "URL",
+                type: "url",
+                description: "" // TODO: url description
+            },
+            {
+                id: "format",
+                label: "Format",
+                type: "text",
+                description: "", // TODO: format description
+                vocabulary: "Format"
+            },
+            {
+                id: "language",
+                label: "Language",
+                type: "text",
+                description: "", // TODO: language description
+                vocabulary: "Language"
+            },
+            {
+                id: "software",
+                label: "Software",
+                type: "text",
+                description: "", // TODO: software description
+                vocabulary: "Software"
+            },
+            {
+                id: "languageWrittenIn",
+                label: "Language written in",
+                type: "text",
+                description: "", // TODO: language written in description
+                vocabulary: "Language_written_in"
+            },
+            {
+                id: "status",
+                label: "Status",
+                type: "text",
+                description: "", // TODO: status description
+                vocabulary: "Status"
+            },
+            {
+                id: "objective",
+                label: "Objective",
+                type: "text",
+                description: "" // TODO: objective description
+            },
+            {
+                id: "description",
+                label: "Description",
+                type: "text",
+                description: "" // TODO: description
+            }
+        ],
+
+        contact: ui.contact,
+        reference: ui.reference,
+
+        scope: [
+            {
+                id: "generalComment",
+                label: "General comment",
+                type: "text",
+                description: "" // TODO: general comment description
+            },
+            {
+                id: "temporalInformation",
+                label: "Temporal information",
+                type: "text",
+                description: "" // TODO: temporal information description
+            },
+            {
+                id: "spatialInformation",
+                label: "Spatial information",
+                type: "text",
+                description: "" // TODO: spatial information description
+            }
+        ],
+        hazard: ui.hazard,
+        populationGroup: ui.populationGroup,
+
+        // Data background
+        study: ui.study,
+        studySample: ui.studySample,
+        laboratory: ui.laboratory,
+        assay: ui.assay,
+
+        // Model math
+        modelMath: [
+            {
+                id: "fittingProcedure",
+                label: "Fitting procedure",
+                type: "text",
+                description: "" // TODO: fitting procedure description
+            },
+            {
+                id: "event",
+                label: "Event",
+                type: "text-array",
+                description: "" // TODO: event description
+            }
+        ],
+        parameter: ui.parameter,
+        qualityMeasures: ui.qualityMeasures,
+        modelEquation: ui.modelEquation,
+        exposure: ui.exposure
+    }
 }
