61f5178171ed97b3f4001e6cb46edcc1906f0ba9
Miguel <miguelalba89@gmail.com>
2019-12-12 17:34:10 +0100

    Add navbar in joiner's body

--- de.bund.bfr.knime.fsklab.nodes/js-src/de/bund/bfr/knime/fsklab/nodes/newjoiner/joiner.js
+++ de.bund.bfr.knime.fsklab.nodes/js-src/de/bund/bfr/knime/fsklab/nodes/newjoiner/joiner.js
@@ -10,6 +10,7 @@ joiner = function() {
         _value = value;
 
         console.log("Hola mundo");
+        createBody();
     }
 
     view.getComponentValue = function() {
@@ -18,4 +19,34 @@ joiner = function() {
     }
 
     return view;
+
+    function createBody() {
+
+        $('body').html(`<div class="container-fluid">
+<nav class="navbar navbar-default">
+ <div class="navbar-collapse collapse">
+   <ul class="nav navbar-nav" id="viewTab">
+     <li role="presentation">
+       <a id="join-tab" href="#joinPanel" aria-controls="joinPanel">Join</a>
+     </li>
+     <li role="presentation">
+       <a id="join-tab" href="#generalInformationPanel" aria-controls="generalInformationPanel">General information</a>
+     </li>
+     <li role="presentation">
+       <a id="join-tab" href="#scopePanel" aria-controls="scopePanel">Scope</a>
+     </li>
+     <li role="presentation">
+       <a id="join-tab" href="#dataBackgroundPanel" aria-controls="dataBackgroundPanel">Data background</a>
+     </li>
+     <li role="presentation">
+       <a id="join-tab" href="#modelMathPanel" aria-controls="modelMathPanel">Model math</a>
+     </li>
+   </ul>
+ </div>
+ <div class="tab-content" id="viewContent">
+   <div role="tabpanel" class="tab-pane" id="joinPanel"></div>
+ </div>
+</nav>
+</div>`);
+    }
 }();
