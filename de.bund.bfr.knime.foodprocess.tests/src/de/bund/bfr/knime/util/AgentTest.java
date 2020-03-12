ec70e67652f26c9c1cc137765a3ed9a717e0933e
Miguel <miguelalba89@gmail.com>
2020-03-12 13:59:32 +0100

    Create AgentTest.java

---
+++ de.bund.bfr.knime.foodprocess.tests/src/de/bund/bfr/knime/util/AgentTest.java
@@ -0,0 +1,30 @@
+package de.bund.bfr.knime.util;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+@SuppressWarnings("static-method")
+public class AgentTest {
+
+	@Test
+	public void testNameConstructor() throws Exception {
+		Agent agent = new Agent("name");
+		assertEquals(-1, agent.getId());
+		assertEquals("name", agent.getName());
+	}
+	
+	@Test
+	public void testNameIdConstructor() throws Exception {
+		Agent agent = new Agent("name", 0);
+		assertEquals(0, agent.getId());
+		assertEquals("name", agent.getName());
+	}
+	
+	@Test
+	public void testIdConstructor() throws Exception {
+		Agent agent = new Agent(4000);
+		assertEquals(4000, agent.getId());
+		// DB name is not tested.
+	}
+}
