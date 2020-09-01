d6d457265a3db0650fd8e37b4090a131bbd5a74c
schuelet <thomas.schueler@bfr.bund.de>
2020-09-01 15:37:07 +0200

    moved fsk2metadata node to de.bund.bfr.knime.fsklab.v1_9.fsk2metadata

--- de.bund.bfr.knime.fsklab.nodes/src/de/bund/bfr/knime/fsklab/v1_9/fsk2metadata/FSK2MetadataNodeModel.java
+++ de.bund.bfr.knime.fsklab.nodes/src/de/bund/bfr/knime/fsklab/v1_9/fsk2metadata/FSK2MetadataNodeModel.java
@@ -16,7 +16,7 @@
  * Contributors: Department Biological Safety - BfR
  ***************************************************************************************************
  */
-package de.bund.bfr.knime.fsklab.nodes;
+package de.bund.bfr.knime.fsklab.v1_9.fsk2metadata;
 
 import java.io.IOException;
 import javax.json.JsonValue;
