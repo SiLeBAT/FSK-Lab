3ef84d0f73ff1b1da4045e77ff8f92de3941c302
miguelalba <miguelalba89@gmail.com>
2020-09-03 17:16:40 +0200

    Move 1.7.2 FSK to metadata node to deprecated project
    #574

---
+++ de.bund.bfr.knime.fsklab.deprecatednodes/src-1_7_2/de/bund/bfr/knime/fsklab/nodes/FSK2MetadataNodeFactory.java
@@ -0,0 +1,54 @@
+/*
+ ***************************************************************************************************
+ * Copyright (c) 2017 Federal Institute for Risk Assessment (BfR), Germany
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program. If
+ * not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contributors: Department Biological Safety - BfR
+ ***************************************************************************************************
+ */
+package de.bund.bfr.knime.fsklab.nodes;
+
+import org.knime.core.node.NodeDialogPane;
+import org.knime.core.node.NodeFactory;
+import org.knime.core.node.NodeView;
+import org.knime.core.node.defaultnodesettings.DefaultNodeSettingsPane;
+import de.bund.bfr.knime.fsklab.nodes.fsk2metadata.FSK2MetadataNodeModel;
+
+public class FSK2MetadataNodeFactory extends NodeFactory<FSK2MetadataNodeModel> {
+
+  @Override
+  public FSK2MetadataNodeModel createNodeModel() {
+    return new FSK2MetadataNodeModel();
+  }
+
+  @Override
+  protected int getNrNodeViews() {
+    return 0;
+  }
+
+  @Override
+  public NodeView<FSK2MetadataNodeModel> createNodeView(int viewIndex,
+      final FSK2MetadataNodeModel nodeModel) {
+    throw new UnsupportedOperationException("No views! " + viewIndex);
+  }
+
+  @Override
+  protected boolean hasDialog() {
+    return false;
+  }
+
+  @Override
+  protected NodeDialogPane createNodeDialogPane() {
+    return new DefaultNodeSettingsPane();
+  }
+}
---
+++ de.bund.bfr.knime.fsklab.deprecatednodes/src-1_7_2/de/bund/bfr/knime/fsklab/nodes/fsk2metadata/FSK2MetadataNodeModel.java
@@ -0,0 +1,123 @@
+/*
+ ***************************************************************************************************
+ * Copyright (c) 2017 Federal Institute for Risk Assessment (BfR), Germany
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program. If
+ * not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contributors: Department Biological Safety - BfR
+ ***************************************************************************************************
+ */
+package de.bund.bfr.knime.fsklab.nodes.fsk2metadata;
+
+import java.io.IOException;
+import javax.json.JsonValue;
+import org.knime.core.data.DataCell;
+import org.knime.core.data.DataColumnSpec;
+import org.knime.core.data.DataColumnSpecCreator;
+import org.knime.core.data.DataTableSpec;
+import org.knime.core.data.DataTableSpecCreator;
+import org.knime.core.data.RowKey;
+import org.knime.core.data.def.DefaultRow;
+import org.knime.core.data.json.JSONCell;
+import org.knime.core.data.json.JSONCellFactory;
+import org.knime.core.node.BufferedDataContainer;
+import org.knime.core.node.BufferedDataTable;
+import org.knime.core.node.ExecutionContext;
+import org.knime.core.node.InvalidSettingsException;
+import org.knime.core.node.StatelessModel;
+import org.knime.core.node.port.PortObject;
+import org.knime.core.node.port.PortObjectSpec;
+import org.knime.core.node.port.PortType;
+import org.knime.json.util.JSONUtil;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.bund.bfr.knime.fsklab.FskPortObject;
+import metadata.SwaggerUtil;
+
+public class FSK2MetadataNodeModel extends StatelessModel {
+
+  private static PortType[] IN_TYPES = {FskPortObject.TYPE};
+  private static PortType[] OUT_TYPES = {BufferedDataTable.TYPE};
+  
+  private static final ObjectMapper MAPPER = new ObjectMapper();
+
+  /**
+   * Constructor for the node model.
+   */
+  public FSK2MetadataNodeModel() {
+    super(IN_TYPES, OUT_TYPES);
+  }
+
+  @Override
+  protected PortObject[] execute(PortObject[] inObjects, ExecutionContext exec) throws Exception {
+
+    // Get model metadata
+    FskPortObject inObj = (FskPortObject) inObjects[0];
+    Object generalInformation = SwaggerUtil.getGeneralInformation(inObj.modelMetadata);
+    Object scope = SwaggerUtil.getScope(inObj.modelMetadata);
+    Object dataBackground = SwaggerUtil.getDataBackground(inObj.modelMetadata);
+    Object modelMath = SwaggerUtil.getModelMath(inObj.modelMetadata);
+
+    // Build container
+    final DataTableSpec tableSpec = createTableSpec()[0];
+    final BufferedDataContainer container = exec.createDataContainer(tableSpec);
+
+    // Create JSON cells
+    final DataCell giCell = createJSONCell(generalInformation);
+    final DataCell scopeCell = createJSONCell(scope);
+    final DataCell dbCell = createJSONCell(dataBackground);
+    final DataCell mathCell = createJSONCell(modelMath);
+
+    // Create and add row to container
+    final DefaultRow row =
+        new DefaultRow(RowKey.createRowKey(0L), giCell, scopeCell, dbCell, mathCell);
+    container.addRowToTable(row);
+
+    container.close();
+
+    return new BufferedDataTable[] {container.getTable()};
+  }
+
+  private static DataCell createJSONCell(final Object object) {
+    JsonValue jsonValue = JsonValue.NULL;
+
+    if (object != null) {
+      try {
+        final String jsonStr = MAPPER.writeValueAsString(object);
+        jsonValue = JSONUtil.parseJSONValue(jsonStr);
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+
+    return JSONCellFactory.create(jsonValue);
+  }
+
+  @Override
+  protected PortObjectSpec[] configure(PortObjectSpec[] inSpecs) throws InvalidSettingsException {
+    return createTableSpec();
+  }
+
+  private DataTableSpec[] createTableSpec() {
+    final DataColumnSpec generalInformationSpec =
+        new DataColumnSpecCreator("generalInformation", JSONCell.TYPE).createSpec();
+    final DataColumnSpec scopeSpec = new DataColumnSpecCreator("scope", JSONCell.TYPE).createSpec();
+    final DataColumnSpec dataBackgroundSpec =
+        new DataColumnSpecCreator("dataBackground", JSONCell.TYPE).createSpec();
+    final DataColumnSpec modelMathSpec =
+        new DataColumnSpecCreator("modelMath", JSONCell.TYPE).createSpec();
+
+    // table spec
+    final DataTableSpecCreator tableSpec = new DataTableSpecCreator()
+        .addColumns(generalInformationSpec, scopeSpec, dataBackgroundSpec, modelMathSpec);
+    return new DataTableSpec[] {tableSpec.createSpec()};
+  }
+}
