7c4a2f653e7f8445d075e79a5311c8f2525d2f4c
SchueleT <thomas.schueler@bfr.bund.de>
2020-09-09 16:30:43 +0200

    unit tests for FskPortObjectUtil

--- de.bund.bfr.knime.fsklab.nodes/src/de/bund/bfr/knime/fsklab/nodes/FskPortObjectUtil.java
+++ de.bund.bfr.knime.fsklab.nodes/src/de/bund/bfr/knime/fsklab/nodes/FskPortObjectUtil.java
@@ -8,36 +8,35 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class FskPortObjectUtil {
 
-  private FskPortObjectUtil() {}
-  
-  
+  private FskPortObjectUtil() {
+  }
+
+
   /**
-   * 
-   * Method to deserialize an object by temporarily setting a new classloader to work around
-   * a deserialization bug in KNIME.
+   * Method to deserialize an object by temporarily setting a new classloader to work around a
+   * deserialization bug in KNIME.
    * 
    * @param <T> type of the object to be deserialized.
    * @param classLoader of the FskPortObject.serializer instance
-   * @param mapper: ObjectMapper 
+   * @param mapper: ObjectMapper
    * @param in : InputStream of object to be deserialized
    * @param valueType: Class of the object to be deserialized.
    * @return deserialized Object
    * @throws IOException
    */
-  public static <T> T deserializeAfterClassloaderReset(ClassLoader classLoader, ObjectMapper mapper, InputStream in, Class<T> valueType)
-      throws IOException
-  {
+  public static <T> T deserializeAfterClassloaderReset(ClassLoader classLoader, ObjectMapper mapper,
+      InputStream in, Class<T> valueType) throws IOException {
 
     // Back up and configure class loader of current thread
     ClassLoader originalClassLoader = Thread.currentThread().getContextClassLoader();
     Thread.currentThread().setContextClassLoader(classLoader);
-    
+
     // deserialize working directory
     T actualManager = mapper.readValue(in, valueType);
-    
+
     // Restore class loader
     Thread.currentThread().setContextClassLoader(originalClassLoader);
-    
+
     return (T) actualManager;
-  }  
+  }
 }
