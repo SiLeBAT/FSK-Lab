501ece12693d89c3f65b20b84dbd776fced18e97
thsch <thsch@DESKTOP-5Q5QQ7E>
2021-03-16 17:54:29 +0100

    need for json serialization exception

---
+++ de.bund.bfr.knime.fsklab.nodes/src/de/bund/bfr/knime/fsklab/JsonSerializationException.java
@@ -0,0 +1,10 @@
+package de.bund.bfr.knime.fsklab;
+
+public class JsonSerializationException extends Exception {
+
+  /**
+   * 
+   */
+  private static final long serialVersionUID = -749790422581895694L;
+
+}
