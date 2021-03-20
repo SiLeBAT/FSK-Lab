6673fcde500a4b6ec072d4196a86ac0ba33bf3cb
Miguel de Alba <miguelalba89@gmail.com>
2021-03-20 02:46:50 +0100

    RAKIP vocabularies loaded from bundle
    The vocabularie were not loaded before straight from the bundle but a second copy included in the classpath as resources. The vocabularies are already included in the vocabularies library but in the Eclipse bundle they do not have easy access as vocabularies.jar is contained in the service bundle (jar in a jar). Because of this, the resources cannot be loaded from the classpath. As a workaround the SQL files used to copied manually to a resources folder named data. These changes extract vocabularies.jar from the bundlle to a temporary file from which the SQL files are read.
    
    Exceptions are implemented for importing the database. Creating the database or its structure fails the import while errors with individual vocabularies do not. In this case the rest of vocabularies are imported.

--- de.bund.bfr.knime.fsklab.service/data/initialdata/laboratory_accreditation.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO laboratory_accreditation (id, name, ssd) VALUES (0, 'Accredited', 'L001A');
-INSERT INTO laboratory_accreditation (id, name, ssd) VALUES (1, 'None', 'L003A');
-INSERT INTO laboratory_accreditation (id, name, ssd) VALUES (2, 'Other', 'Other');
-INSERT INTO laboratory_accreditation (id, name, ssd) VALUES (3, 'Third party assessment', 'L002A');
--- de.bund.bfr.knime.fsklab.service/data/initialdata/parameter_source.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO parameter_source (id, name) VALUES (0, 'Article');
-INSERT INTO parameter_source (id, name) VALUES (1, 'Assumption');
-INSERT INTO parameter_source (id, name) VALUES (2, 'Data');
-INSERT INTO parameter_source (id, name) VALUES (3, 'Estimate');
-INSERT INTO parameter_source (id, name) VALUES (4, 'Expert opinion');
-INSERT INTO parameter_source (id, name) VALUES (5, 'Model result');
-INSERT INTO parameter_source (id, name) VALUES (6, 'Not applicable ');
-INSERT INTO parameter_source (id, name) VALUES (7, 'Other');
