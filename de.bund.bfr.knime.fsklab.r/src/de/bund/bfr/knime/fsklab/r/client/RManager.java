46e059ccfa19b530ea90a563e4f52edc5c969cd3
Ahmad Swaid <ahmadswaid@gmail.com>
2021-04-02 18:44:26 +0200

    support other OS systems

--- de.bund.bfr.knime.fsklab.r/src/de/bund/bfr/knime/fsklab/r/client/RManager.java
+++ de.bund.bfr.knime.fsklab.r/src/de/bund/bfr/knime/fsklab/r/client/RManager.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.locks.ReentrantLock;
-
+import org.apache.commons.lang3.SystemUtils;
 import de.bund.bfr.knime.fsklab.r.server.RConnectionFactory;
 
 public class RManager {
@@ -12,7 +12,9 @@ public class RManager {
   private static int rHandlerCount;
   private static ReentrantLock lock = new ReentrantLock();
   private static Map<Integer, String> rProcessID = new HashMap<>();
-  private static final String KILL = "taskkill /F /IM ";
+  private static final String KILL_OS_WIN = "taskkill /F /IM ";
+  private static final String KILL_OS_OTHER = "KILL -9";
+  
   
   public static void storeExecutorProcessID(int executorHash, String pID) throws IOException {
       rProcessID.put(executorHash, pID);
@@ -48,8 +50,12 @@ public class RManager {
   public static void killProcess(String serviceName) {
 
 		try {
-			Runtime.getRuntime().exec(KILL + serviceName);
-			System.out.println(serviceName+" killed successfully!");
+		    if(SystemUtils.IS_OS_WINDOWS)
+		      Runtime.getRuntime().exec(KILL_OS_WIN + serviceName);
+		    else {
+		      Runtime.getRuntime().exec(KILL_OS_OTHER + serviceName);
+			  System.out.println(serviceName+" killed successfully! ");
+		    }
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
