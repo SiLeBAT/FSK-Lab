f051530e07dd4cc20a3143ccd5dd4bce26fc9ccb
Miguel de Alba <miguelalba89@gmail.com>
2021-05-05 11:23:50 +0200

    Development update site is split (#898)

--- gitlab_development.sh
+++ gitlab_development.sh
@@ -1,11 +1,11 @@
 TARGET_FOLDER="$TRAVIS_BUILD_DIR/de.bund.bfr.knime.update/target"
 
 # Check Gitlab repo
-git clone https://$GITLAB_USER:$GITLAB_TOKEN@gitlab.bfr.berlin/silebat/repositories.git
-rm -Rf repositories/development # Deletes old build if it exists
-mv $TARGET_FOLDER/repository $TARGET_FOLDER/development
-mv $TARGET_FOLDER/development repositories/development
-cd repositories/development
+git clone https://$GITLAB_USER:$GITLAB_TOKEN@gitlab.bfr.berlin/silebat/development.git
+rm -Rf repositories/update # Deletes old build if it exists
+mv $TARGET_FOLDER/repository $TARGET_FOLDER/update
+mv $TARGET_FOLDER/update repositories/update
+cd repositories/update
 git add .
 git commit -m "Release"
 git push https://$GITLAB_USER:$GITLAB_TOKEN@gitlab.bfr.berlin/silebat/repositories.git --all
