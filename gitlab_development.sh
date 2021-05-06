39eaf90b13126b0bdd329db1c22bb229541a89d9
Miguel de Alba <miguelalba89@gmail.com>
2021-05-06 13:13:42 +0200

    Split dev site (#899)

--- gitlab_development.sh
+++ gitlab_development.sh
@@ -1,11 +1,16 @@
 TARGET_FOLDER="$TRAVIS_BUILD_DIR/de.bund.bfr.knime.update/target"
+REPO="development"
 
 # Check Gitlab repo
-git clone https://$GITLAB_USER:$GITLAB_TOKEN@gitlab.bfr.berlin/silebat/development.git
-rm -Rf repositories/update # Deletes old build if it exists
+git clone https://$GITLAB_USER:$GITLAB_TOKEN@gitlab.bfr.berlin/silebat/$REPO.git
+
+# Update build
+rm -Rf $REPO/update # Deletes old build if it exists
 mv $TARGET_FOLDER/repository $TARGET_FOLDER/update
-mv $TARGET_FOLDER/update repositories/update
-cd repositories/update
+mv $TARGET_FOLDER/update $REPO/update
+cd $REPO/update
 git add .
 git commit -m "Release"
+
+# Push build
 git push https://$GITLAB_USER:$GITLAB_TOKEN@gitlab.bfr.berlin/silebat/repositories.git --all
