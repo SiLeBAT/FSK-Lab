820da9034bae27880d91fd28d97e97ac93de76a4
thschuel <thomas.schueler@bfr.bund.de>
2021-05-07 15:36:51 +0200

    update gitlab script

--- gitlab_development.sh
+++ gitlab_development.sh
@@ -5,12 +5,13 @@ REPO="development"
 git clone https://$GITLAB_USER:$GITLAB_TOKEN@gitlab.bfr.berlin/silebat/$REPO.git
 
 # Update build
-rm -Rf $REPO/update # Deletes old build if it exists
-mv $TARGET_FOLDER/repository $TARGET_FOLDER/update
-mv $TARGET_FOLDER/update $REPO/update
-cd $REPO/update
+rm -Rf $REPO/fsklab # Deletes old build if it exists
+
+mv $TARGET_FOLDER/repository $TARGET_FOLDER/fsklab
+mv $TARGET_FOLDER/fsklab $REPO/fsklab
+cd $REPO/fsklab
 git add .
-git commit -m "Release"
+git commit -m "Development"
 
 # Push build
 git push https://$GITLAB_USER:$GITLAB_TOKEN@gitlab.bfr.berlin/silebat/$REPO.git --all
