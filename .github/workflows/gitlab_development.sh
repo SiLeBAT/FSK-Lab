707f40251c5699c095937e9b1db4bbac75644c01
glpat-L-KoeS5FDxM-7bnUacre <thomas.schueler@bfr.bund.de>
2023-01-05 12:53:48 +0100

    fix second try

--- .github/workflows/gitlab_development.sh
+++ .github/workflows/gitlab_development.sh
@@ -1,12 +1,9 @@
 TARGET_FOLDER="$GITHUB_WORKSPACE/de.bund.bfr.knime.update/target"
 REPO="development"
 
-git config --global user.email ${{secrets.GITLAB_EMAIL}}
-git config --global user.name ${{secrets.GITLAB_TOKEN}}
-git commit --amend --reset-author
 
 # Check Gitlab repo
-git clone -b 4.5 --single-branch --depth=1 https://gitlab.bfr.berlin/silebat/$REPO.git
+git clone -b 4.5 --single-branch --depth=1 https://${{secrets.GITLAB_USER}}:${{secrets.GITLAB_TOKEN}}@gitlab.bfr.berlin/silebat/$REPO.git
 
 # Update build
 rm -Rf $REPO/fsklab # Deletes old build if it exists
@@ -14,6 +11,10 @@ rm -Rf $REPO/fsklab # Deletes old build if it exists
 mv $TARGET_FOLDER/repository $TARGET_FOLDER/fsklab
 mv $TARGET_FOLDER/fsklab $REPO/fsklab
 cd $REPO/fsklab
+
+git config --global user.email ${{secrets.GITLAB_EMAIL}}
+git config --global user.name ${{secrets.GITLAB_TOKEN}}
+
 
 git add .
 git commit -m "Development 4.5"
