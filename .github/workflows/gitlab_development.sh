afda79ebd894693ffe958cd81ffd23f835612fc6
glpat-L-KoeS5FDxM-7bnUacre <thomas.schueler@bfr.bund.de>
2023-01-05 11:59:43 +0100

    fix for use of github variables

--- .github/workflows/gitlab_development.sh
+++ .github/workflows/gitlab_development.sh
@@ -1,12 +1,12 @@
 TARGET_FOLDER="$GITHUB_WORKSPACE/de.bund.bfr.knime.update/target"
 REPO="development"
 
-git config --global user.email $GITLAB_EMAIL
-git config --global user.name $GITLAB_TOKEN
+git config --global user.email ${{secrets.GITLAB_EMAIL}}
+git config --global user.name ${{secrets.GITLAB_TOKEN}}
 git commit --amend --reset-author
 
 # Check Gitlab repo
-git clone -b 4.5 --single-branch --depth=1 https://$GITLAB_USER:$GITLAB_TOKEN@gitlab.bfr.berlin/silebat/$REPO.git
+git clone -b 4.5 --single-branch --depth=1 https://gitlab.bfr.berlin/silebat/$REPO.git
 
 # Update build
 rm -Rf $REPO/fsklab # Deletes old build if it exists
@@ -19,4 +19,4 @@ git add .
 git commit -m "Development 4.5"
 
 # Push build
-git push https://$GITLAB_USER:$GITLAB_TOKEN@gitlab.bfr.berlin/silebat/$REPO.git 4.5
+git push https://${{secrets.GITLAB_USER}}:${{secrets.GITLAB_TOKEN}}@gitlab.bfr.berlin/silebat/$REPO.git 4.5
