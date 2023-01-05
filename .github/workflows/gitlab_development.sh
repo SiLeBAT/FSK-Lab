469496a851b3e535f27324eeb062cc182defd7a5
glpat-L-KoeS5FDxM-7bnUacre <thomas.schueler@bfr.bund.de>
2023-01-05 11:08:55 +0100

    trying to fix the gitlab authentification issue

--- .github/workflows/gitlab_development.sh
+++ .github/workflows/gitlab_development.sh
@@ -1,8 +1,12 @@
 TARGET_FOLDER="$GITHUB_WORKSPACE/de.bund.bfr.knime.update/target"
 REPO="development"
 
+git config --global user.email $GITLAB_EMAIL
+git config --global user.name $GITLAB_TOKEN
+git commit --amend --reset-author
+
 # Check Gitlab repo
-git clone -b 4.5 https://$GITLAB_USER:$GITLAB_TOKEN@gitlab.bfr.berlin/silebat/$REPO.git
+git clone -b 4.5 --single-branch --depth=1 https://$GITLAB_USER:$GITLAB_TOKEN@gitlab.bfr.berlin/silebat/$REPO.git
 
 # Update build
 rm -Rf $REPO/fsklab # Deletes old build if it exists
@@ -10,11 +14,9 @@ rm -Rf $REPO/fsklab # Deletes old build if it exists
 mv $TARGET_FOLDER/repository $TARGET_FOLDER/fsklab
 mv $TARGET_FOLDER/fsklab $REPO/fsklab
 cd $REPO/fsklab
-git config --global user.email $GITLAB_EMAIL
-git config --global user.name $GITLAB_TOKEN
 
 git add .
-git commit -m "Development"
+git commit -m "Development 4.5"
 
 # Push build
 git push https://$GITLAB_USER:$GITLAB_TOKEN@gitlab.bfr.berlin/silebat/$REPO.git 4.5
