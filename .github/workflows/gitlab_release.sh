45b5667713705f833e34f2b6fd4167d29ac7bd53
Thomas Schueler <thomas.schueler@bfr.bund.de>
2023-03-16 15:58:46 +0100

    change message to reflect the KNIME update-site

--- .github/workflows/gitlab_release.sh
+++ .github/workflows/gitlab_release.sh
@@ -15,7 +15,7 @@ git config user.email $GITLAB_EMAIL
 git config user.name $GITLAB_NAME
 
 git add .
-git commit -m "https://update.knime.com/community-contributions/trunk"
+git commit -m "https://update.knime.com/community-contributions/4.5"
 
 # Push build
 git push https://$GITLAB_NAME:$GITLAB_TOKEN@gitlab.bfr.berlin/4SZ/$REPO.git 4.5
